4aa2173604f646e95cf0fd1d4f011c4c7602e113
Eduardo Ludi <eduludi@gmail.com>
2017-12-06 10:06:57 +0100

    Adds Knobs control to Button and Icon stories.

--- src/atoms/buttons/Button.stories.js
+++ src/atoms/buttons/Button.stories.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
 
 import Button from './Button'
+import { iconNames } from '../icons/Icon'
 
-storiesOf('Button', module)
-  .add('with text', () => <Button url="http://adventist.io" text="Hello" />)
-  .add('with share icon', () => <Button url="#" text="Share" icon="share" />)
-  .add('with world icon', () => (
-    <Button url="#" text="Hello world" icon="world" />
-  ))
+const stories = storiesOf('Button', module)
+stories.addDecorator(withKnobs)
+
+stories.add('only text', () => {
+  const textValue = text('Text', 'Learn More')
+  return <Button url="http://adventist.io" text={textValue} />
+})
+stories.add('with text and icon', () => {
+  const textValue = text('Text', 'Learn More')
+  const icon = select('Icon', iconNames, 'share')
+  return <Button url="#" text={textValue} icon={icon} />
+})
--- src/atoms/icons/Icon.stories.js
+++ src/atoms/icons/Icon.stories.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-// import { action } from '@storybook/addon-actions'
+import { withKnobs, select } from '@storybook/addon-knobs'
 
-import Icon from './Icon'
+import Icon, { iconNames, iconColors, iconSizes } from './Icon'
 
-storiesOf('Icon', module)
-  .add('Share "blue"', () => <Icon name="share" color="blue" />)
-  .add('Share "gray"', () => <Icon name="share" color="gray" />)
-  .add('Share "light-brown"', () => <Icon name="share" color="light-brown" />)
-  .add('Share "white"', () => (
-    <div style={{ backgroundColor: 'gray' }}>
-      <Icon name="share" color="white" />
+const stories = storiesOf('Icon', module)
+
+stories.addDecorator(withKnobs)
+
+stories.add('default', () => {
+  const name = select('Name', iconNames, 'logo')
+  const color = select('Color', iconColors, 'fill--blue')
+  const size = select('Size', iconSizes, 'xl')
+  return (
+    <div style={{ backgroundColor: '#eee', padding: '1em' }}>
+      <Icon name={name} size={size} color={color} />
     </div>
-  ))
-  .add('World "xs" size ', () => <Icon name="world" size="xs" />)
-  .add('World "s" size ', () => <Icon name="world" size="s" />)
-  .add('World "m" size ', () => <Icon name="world" size="m" />)
-  .add('World "l" size ', () => <Icon name="world" size="l" />)
-  .add('World "xl" size ', () => <Icon name="world" size="xl" />)
-  .add('World "xl blue" ', () => <Icon name="world" size="xl" color="blue" />)
+  )
+})
