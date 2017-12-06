1a78c024b82d215b3840c3057da95fad5c0439ad
Eduardo Ludi <eduludi@gmail.com>
2017-12-06 13:14:58 +0100

    Adds atom/images/Picture stories

---
+++ src/atoms/images/Picture.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+// import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean, object } from '@storybook/addon-knobs'
+
+import Picture from './Picture'
+
+const defaultImage = {
+  default: '//placehold.it/250x100',
+  500: '//placehold.it/500x200',
+  1000: '//placehold.it/1000x400',
+  1500: '//placehold.it/1500x600',
+}
+const defaultAlt = 'Just a placeholder image'
+
+const stories = storiesOf('atoms/images/Picture', module)
+stories.addDecorator(withKnobs)
+
+stories.add('with image and alt', () => {
+  const image = object('image', defaultImage)
+  const alt = text('alt', defaultAlt)
+  return <Picture image={image} alt={alt} />
+})
+
+stories.add('with lazy', () => {
+  const image = object('image', defaultImage)
+  const alt = text('alt', defaultAlt)
+  const lazy = boolean('lazy', true)
+  return <Picture image={image} alt={alt} lazy={lazy} />
+})
