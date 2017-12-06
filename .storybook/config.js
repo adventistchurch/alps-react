84f679b2fc24f274e6fae5bf345dc3fb272591f3
Eduardo Ludi <eduludi@gmail.com>
2017-12-06 17:42:17 +0100

    Adds knobs to handle primary and secondary colors in every component.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
+import { withKnobs, select } from '@storybook/addon-knobs'
+
+import * as colors from '../src/atoms/global/colors'
 
 const style = {
   margin: '1em',
 }
 
-addDecorator(story => <div {...{ style }}>{story()}</div>)
+addDecorator(story => {
+  const content = story()
+  const primaryColor = select('Primary color', colors.primary, 'denim')
+  const secondaryColor = select('Secondary color', colors.secondary, 'warm')
+  return (
+    <div className={`theme--${primaryColor}`}>
+      <div className={`theme--${secondaryColor}`} {...{ style }}>
+        {content}
+      </div>
+    </div>
+  )
+})
+
+addDecorator(withKnobs) // this must be added after the story decorator
 
 const req = require.context('../src', true, /\.stories\.js$/)
 
