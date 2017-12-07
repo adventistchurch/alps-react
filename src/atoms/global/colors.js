5444a44713151eb7e24130b78ba5f610861ba91d
Eduardo Ludi <eduludi@gmail.com>
2017-12-07 15:52:08 +0100

    Adds thems and fill colors.

--- src/atoms/global/colors.js
+++ src/atoms/global/colors.js
@@ -2,4 +2,24 @@ const primary = ['emperor', 'earth', 'grapevine', 'denim', 'campfire']
 
 const secondary = ['warm', 'cool']
 
-export { primary, secondary }
+const theme = {
+  primary: {
+    fill: 'theme--primary-fill-color',
+    text: 'theme--primary-text-color',
+  },
+  secondary: {
+    fill: 'theme--secondary-fill-color',
+    text: 'theme--secondary-text-color',
+  },
+}
+
+const fill = [
+  'fill--blue',
+  'fill--light-brown',
+  'fill--gray',
+  'fill--white',
+  theme.primary.fill,
+  theme.secondary.fill,
+]
+
+export { primary, secondary, theme, fill }
