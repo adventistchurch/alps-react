eaaf2c766ff4f3fbc95d4e91e6d7f91da237ba79
Eduardo Ludi <eduludi@gmail.com>
2017-12-07 15:51:17 +0100

    Updates Storybook config to set background color to decorator.

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,18 +5,25 @@ import { withKnobs, select } from '@storybook/addon-knobs'
 import * as colors from '../src/atoms/global/colors'
 
 const style = {
-  margin: '1em',
+  padding: '1em',
+  height: '100vh',
 }
 
 addDecorator(story => {
   const content = story()
   const primaryColor = select('Primary color', colors.primary, 'denim')
   const secondaryColor = select('Secondary color', colors.secondary, 'warm')
+  const backgroundColor = select(
+    'Background color',
+    ['black', 'white'],
+    'white'
+  )
   return (
-    <div className={`theme--${primaryColor}`}>
-      <div className={`theme--${secondaryColor}`} {...{ style }}>
-        {content}
-      </div>
+    <div
+      className={`theme--${primaryColor}`}
+      style={{ backgroundColor, ...style }}
+    >
+      <div className={`theme--${secondaryColor}`}>{content}</div>
     </div>
   )
 })
