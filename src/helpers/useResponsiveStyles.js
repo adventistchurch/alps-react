09ffb287bbb41dda94832303f3da107f14443659
Eduardo Ludi <eduludi@gmail.com>
2019-02-13 16:45:25 +0100

    Refactors and adds JSDocs comments to utils/useResponsiveStyles

--- src/helpers/useResponsiveStyles.js
+++ src/helpers/useResponsiveStyles.js
@@ -1,4 +1,9 @@
 // Given a rule, wraps it in a media query
+/**
+ *
+ * @param {string} rule - A CSS rule that can be wrapped
+ * @param {string} query - A media query expressions (`min-width`, `max-with`, etc.)
+ */
 function wrapMediaQuery(rule, query) {
   return `
     ${query ? `@media(${query}) {` : ''}
@@ -7,12 +12,17 @@ function wrapMediaQuery(rule, query) {
     `
 }
 
-// returns a string of css rules wraped in queries for the provided media queries
-function useResponsiveStyles(rule, data = {}, mediaQueries = []) {
-  const styles = Object.keys(mediaQueries).map(name => {
-    const query = mediaQueries[name]
-    return wrapMediaQuery(rule(data[name]), query)
-  })
+/**
+ * Returns a string of css rules wraped in queries for the provided media queries
+ *
+ * @param {function} rule - Function that returns a CSS rule as string
+ * @param {Map} data - Map with data that will be used by `rule()` for a specific media query
+ * @param {Map} mediaQueries - Map of media queries to be looped through
+ */
+function useResponsiveStyles(rule, data = {}, mediaQueries = {}) {
+  const styles = Object.keys(mediaQueries).map(mq =>
+    wrapMediaQuery(rule(data[mq]), mediaQueries[mq])
+  )
 
   return styles.join('')
 }
