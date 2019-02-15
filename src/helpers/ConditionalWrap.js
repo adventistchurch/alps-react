81265edb0dd8954d6e344fbd5ce3edc3978c6c1f
Eduardo Ludi <eduludi@gmail.com>
2019-02-15 09:44:27 +0100

    Renames ConditionalWrap to WrapIf

--- src/helpers/ConditionalWrap.js
+++
@@ -1,4 +0,0 @@
-const ConditionalWrap = ({ condition, wrap, wrapElse, children }) =>
-  wrap && condition ? wrap(children) : wrapElse ? wrapElse(children) : children
-
-export default ConditionalWrap
