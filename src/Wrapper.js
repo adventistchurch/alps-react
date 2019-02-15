92c158715e78d696c6de3ca3dff70aa8a087473f
Eduardo Ludi <eduludi@gmail.com>
2019-02-15 13:14:18 +0100

    Adds MenuContext to Wrapper

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 // import LoadScript from './helpers/LoadScript'
 
+const MenuContext = React.createContext({
+  menuIsOpen: false,
+  menuWithSearch: false,
+  setMenuIsOpen: value => !value,
+})
+
 function Wrapper({ children, primaryColor, secondaryColor, style }) {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const [menuWithSearch, setMenuWithSearch] = useState(false)
   return (
-    <div className={`u-theme--${primaryColor}`} style={style}>
-      <div className={`body has-grid u-theme--${secondaryColor}`}>
-        {children}
+    <MenuContext.Provider
+      value={{ menuIsOpen, setMenuIsOpen, menuWithSearch, setMenuWithSearch }}
+    >
+      <div className={`u-theme--${primaryColor}`} style={style}>
+        <div className={`body has-grid u-theme--${secondaryColor}`}>
+          {children}
+        </div>
+        {/* <LoadScript url="https://code.jquery.com/jquery-2.2.4.min.js" /> */}
+        {/* <LoadScript url="//cdn.adventist.org/alps/3/latest/js/script.min.js" /> */}
       </div>
-      {/* <LoadScript url="https://code.jquery.com/jquery-2.2.4.min.js" /> */}
-      {/* <LoadScript url="//cdn.adventist.org/alps/3/latest/js/script.min.js" /> */}
-    </div>
+    </MenuContext.Provider>
   )
 }
 
@@ -25,5 +37,7 @@ Wrapper.defaultProps = {
   primaryColor: 'ming',
   secondaryColor: '',
 }
+
+export { MenuContext }
 
 export default Wrapper
