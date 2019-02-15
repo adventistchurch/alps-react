ed4364befbda63f95e9f5e090a212b5a56a54057
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-15 09:32:03 +0100

    Adds RelatedPosts to BasicPage.

--- src/templates/BasicPage.js
+++ src/templates/BasicPage.js
@@ -2,44 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Aside from '../organisms/asides/Aside'
+import RelatedPosts from '../organisms/asides/RelatedPosts'
 import BreakoutBlock from '../molecules/blocks/BreakoutBlock'
 import PageHeader from '../organisms/sections/PageHeader'
 import PageContent from '../organisms/content/PageContent'
 
 const BasicPage = ({
-  title,
-  kicker,
   background,
-  content,
   breadcrumbs,
+  kicker,
+  title,
+  content,
   breakout,
   aside,
+  relatedPosts,
 }) => {
-  const sectionClass = `l-main__content l-grid l-grid--7-col u-shift--left--1-col--at-${
-    breakout || aside ? 'xxlarge' : 'large'
-  } l-grid-wrap--6-of-7 u-spacing--double--until-xxlarge u-padding--zero--sides`
-
-  const articleClass = `c-article l-grid-item l-grid-item--l--4-col ${
-    breakout || aside ? 'l-grid-item--xl--3-col' : ''
-  }`
-
+  const hasSidebar = breakout || aside || relatedPosts
   return (
     <>
       <main
         className="l-main u-spacing--double u-padding--double--bottom"
         role="main"
       >
-        <PageHeader {...{ title, kicker, breadcrumbs, background }} />
-        <section id="top" className={sectionClass}>
-          <article className={articleClass}>
+        <PageHeader {...{ background, breadcrumbs, kicker, title }} />
+        <section
+          id="top"
+          className={`l-main__content l-grid l-grid--7-col u-shift--left--1-col--at-${
+            hasSidebar ? 'xxlarge' : 'large'
+          } l-grid-wrap--6-of-7 u-spacing--double--until-xxlarge u-padding--zero--sides`}
+        >
+          <article
+            className={`c-article l-grid-item l-grid-item--l--4-col ${
+              hasSidebar ? 'l-grid-item--xl--3-col' : ''
+            }`}
+          >
             <div className="c-article__body">
               <PageContent breadcrumbs={breadcrumbs}>{content}</PageContent>
             </div>
           </article>
-          {(breakout || aside) && (
+          {hasSidebar && (
             <div className="c-sidebar l-grid-item l-grid-item--l--2-col l-grid-item--xl--2-col u-spacing u-padding--zero--sides">
               {breakout && <BreakoutBlock {...breakout} />}
-              <Aside>{aside}</Aside>
+              {aside && <Aside {...aside} />}
+              {relatedPosts && <RelatedPosts {...relatedPosts} />}
             </div>
           )}
         </section>
@@ -57,6 +62,7 @@ BasicPage.propTypes = {
   breadcrumbs: PageContent.propTypes.breadcrumbs,
   breakout: PropTypes.shape(BreakoutBlock.propTypes),
   aside: PropTypes.oneOfType([PropTypes.element]),
+  relatedPosts: PropTypes.oneOfType([PropTypes.element]),
 }
 BasicPage.defaultProps = {
   breadcrumbs: [],
