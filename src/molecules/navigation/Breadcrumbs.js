a63473aa2d7a5bc0249559d79b98b4586b85c5da
Eduardo Ludi <eduludi@gmail.com>
2019-02-15 10:37:13 +0100

    Adds stories data in a JSON file for Breadcrumbs.
    Also adds story with levels and does some minor refactoring.

--- src/molecules/navigation/Breadcrumbs.js
+++ src/molecules/navigation/Breadcrumbs.js
@@ -4,36 +4,41 @@ import PropTypes from 'prop-types'
 import Icon from '../../atoms/icons/Icon'
 import renderItems from '../../helpers/renderItems'
 
-const BreadcrumbItem = ({ text, url }) => (
-  <li className="c-breadcrumbs__list-item u-font--secondary--s u-text-transform--upper u-display--inline-block u-color--gray">
-    <span className="u-icon u-icon--xs u-path-fill--gray">
-      <Icon name="arrow-bracket-right" />
-    </span>
-    <strong>
-      {url ? (
-        <a href={url} className="c-breadcrumbs__link can-be--white">
-          {text}
-        </a>
-      ) : (
-        text
-      )}
-    </strong>
-  </li>
-)
+function BreadcrumbItem({ text, url }) {
+  return (
+    <li className="c-breadcrumbs__list-item u-font--secondary--s u-text-transform--upper u-display--inline-block u-color--gray">
+      <span className="u-icon u-icon--xs u-path-fill--gray">
+        <Icon name="arrow-bracket-right" />
+      </span>
+      <strong>
+        {url ? (
+          <a href={url} className="c-breadcrumbs__link can-be--white">
+            {text}
+          </a>
+        ) : (
+          text
+        )}
+      </strong>
+    </li>
+  )
+}
 
 BreadcrumbItem.propTypes = {
   text: PropTypes.string.isRequired,
   url: PropTypes.string,
 }
 
-const Breadcrumbs = ({ items }) =>
-  items.length > 0 && (
-    <nav className="c-breadcrumbs" role="navigation">
-      <ul className="c-breadcrumbs__list">
-        {renderItems(items, BreadcrumbItem)}
-      </ul>
-    </nav>
+function Breadcrumbs({ items }) {
+  return (
+    items.length > 0 && (
+      <nav className="c-breadcrumbs" role="navigation">
+        <ul className="c-breadcrumbs__list">
+          {renderItems(items, BreadcrumbItem)}
+        </ul>
+      </nav>
+    )
   )
+}
 
 Breadcrumbs.propTypes = {
   items: PropTypes.array,
--- src/molecules/navigation/Breadcrumbs.stories.js
+++ src/molecules/navigation/Breadcrumbs.stories.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { array, withKnobs } from '@storybook/addon-knobs'
+import { object, select, withKnobs } from '@storybook/addon-knobs'
 
 import Breadcrumbs from './Breadcrumbs'
 
+import data from './Breadcrumbs.stories.json'
+
 const propsTab = 'Props'
-const defaults = {
-  breadcrumbs: [
-    { text: 'Home', url: '/' },
-    { text: 'Vitality', url: '/vitality' },
-    { text: 'Photos' },
-  ],
-}
 
 storiesOf('molecules/navigation/Breadcrumbs', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const breadcrumbs = array(
-      'Breadcrumbs',
-      defaults.breadcrumbs,
-      ',',
-      propsTab
-    )
-    return <Breadcrumbs items={breadcrumbs} />
+    const items = object('Items ', data.items, propsTab)
+    return <Breadcrumbs items={items} />
+  })
+
+  .addWithJSX('Levels', () => {
+    const levels = data.items.map((i, k) => k + 1)
+    const level = select('Levels', levels, 3, propsTab)
+    const items = data.items.slice(0, levels[level - 1])
+    items[level - 1].url = undefined // makes sure that last item has no url
+    return <Breadcrumbs items={items} />
   })
