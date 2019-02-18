69e7a9e6d29aac8aeacfe0144afa4b10f13cad12
Eduardo Ludi <eduludi@gmail.com>
2019-02-18 15:49:42 +0100

    Adds atoms/text/Caption and its stories

---
+++ src/atoms/texts/Caption.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Caption({ children, text }) {
+  return (
+    <div className="o-caption u-color--gray u-font--secondary--s">
+      {children || text}
+    </div>
+  )
+}
+
+Caption.propTypes = {
+  children: PropTypes.node,
+  text: PropTypes.string,
+}
+Caption.defaultProps = {
+  text: '',
+}
+
+export default Caption
