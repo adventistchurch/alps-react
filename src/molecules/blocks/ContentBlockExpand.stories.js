4c8326fd2dfe48abfd45564f8c8faf515323e92d
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-19 14:56:24 +0100

    Decouples test data from ContentBlockExpand story. Adds asterisk to required fields in ContentBlockExpand stories.

--- src/molecules/blocks/ContentBlockExpand.stories.js
+++ src/molecules/blocks/ContentBlockExpand.stories.js
@@ -4,21 +4,17 @@ import { withKnobs, text } from '@storybook/addon-knobs'
 
 import ContentBlockExpand from './ContentBlockExpand'
 
+import data from './ContentBlockExpand.stories.json'
+
 const propsTab = 'Props'
-const defaults = {
-  kicker: 'The General Conference.',
-  title: 'Coordinates the global ministry of the Seventh-day Adventist Church.',
-  description:
-    'Is responsible for the spiritual and developmental plans of the church around the world.',
-}
 
 storiesOf('molecules/blocks/ContentBlockExpand', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const kicker = text('Kicker', defaults.kicker, propsTab)
-    const title = text('Title', defaults.title, propsTab)
-    const description = text('Description', defaults.description, propsTab)
+    const title = text('Title *', data.title, propsTab)
+    const kicker = text('Kicker', data.kicker, propsTab)
+    const description = text('Description', data.description, propsTab)
     return (
       <ContentBlockExpand
         kicker={kicker}
