9145583db3e3013f7770d35e6e09e39c9672f048
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-19 11:48:09 +0100

    Changes Video component to a function

--- src/atoms/media/Video.js
+++ src/atoms/media/Video.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Video = ({ src, width, height }) => {
+function Video({ src, width, height }) {
   return (
     <iframe
       src={src}
