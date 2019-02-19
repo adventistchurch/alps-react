b1cb59189982ead0ad7acaa52d0fd81e960ed0ae
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-19 14:56:56 +0100

    Decouples test data from BreakoutBlock stories.

--- src/molecules/blocks/GalleryBlock.stories.js
+++ src/molecules/blocks/GalleryBlock.stories.js
@@ -4,62 +4,17 @@ import { withKnobs, text, object } from '@storybook/addon-knobs'
 
 import GalleryBlock from './GalleryBlock'
 
-const defaults = {
-  kicker: 'The General Conference.',
-  title: 'Coordinates the global ministry of the Seventh-day Adventist Church.',
-  description:
-    'Is responsible for the spiritual and developmental plans of the church around the world.',
-  images: [
-    {
-      srcSet: {
-        default: '//picsum.photos/500/282?image=1067',
-        500: '//picsum.photos/900/507',
-        750: '//picsum.photos/700/395',
-        1200: '//picsum.photos/500/282',
-      },
-      alt: 'Random image',
-      caption: 'Nam pretium et mauris sit amet vehicula.',
-    },
-    {
-      srcSet: {
-        default: '//picsum.photos/500/282?image=464',
-        500: '//picsum.photos/900/507',
-        750: '//picsum.photos/700/395',
-        1200: '//picsum.photos/500/282',
-      },
-      alt: 'First image',
-      caption: 'Morbi at est tristique.',
-    },
-    {
-      srcSet: {
-        default: '//picsum.photos/500/700?image=534',
-        500: '//picsum.photos/300/400',
-        750: '//picsum.photos/450/600',
-        1200: '//picsum.photos/600/800',
-      },
-      alt: 'Second image portrait',
-      caption: 'Duis eu pharetra magna.',
-      portrait: true,
-    },
-    {
-      srcSet: {
-        default: '//picsum.photos/500/282?image=201',
-        500: '//picsum.photos/900/507',
-        750: '//picsum.photos/700/395',
-        1200: '//picsum.photos/500/282',
-      },
-      alt: 'Third image',
-      caption: 'Curabitur vulputate mattis dignissim.',
-    },
-  ],
-}
+import data from './GalleryBlock.stories.json'
+
+const propsTab = 'Props'
+const imagesTab = 'Images'
 
 storiesOf('molecules/blocks/GalleryBlock', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const kicker = text('Kicker', defaults.kicker)
-    const title = text('Title', defaults.title)
-    const images = object('Image SrcSet', defaults.images)
+    const kicker = text('Kicker', data.kicker, propsTab)
+    const title = text('Title', data.title, propsTab)
+    const images = object('Image SrcSet', data.images, imagesTab)
     return <GalleryBlock images={images} kicker={kicker} title={title} />
   })
