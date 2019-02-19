e1b00a8096fe4b7fcde398914ccbc71164cad996
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-19 14:55:54 +0100

    Decouples test data from ContentBlock stories. Adds asterisk to required fields in ContentBlock stories. Adds showImage flag to ContentBlock stories.

--- src/molecules/blocks/ContentBlock.stories.js
+++ src/molecules/blocks/ContentBlock.stories.js
@@ -10,40 +10,11 @@ import {
 
 import ContentBlock from './ContentBlock'
 
+import data from './ContentBlock.stories.json'
+
 const propsTab = 'Props'
 const ctaTab = 'CTA'
 const imageTab = 'Image'
-const defaults = {
-  title: 'General Conference',
-  category: '',
-  description:
-    'The General Conference coordinates the global ministry of the Seventh-day Adventist Church and is responsible for the spiritual and developmental plans of the church around the world.',
-  more:
-    'Seventh-day Adventist beliefs are meant to permeate your whole life. Growing out of scriptures that paint a compelling portrait of God, you are invited to explore, experience and know the One who desires to make us whole.',
-  cta: 'Find out more',
-  url: 'https://www.adventist.org',
-  imageSrcSet: {
-    Landscape: {
-      default: '//picsum.photos/500/375',
-      500: '//picsum.photos/900/700',
-      750: '//picsum.photos/700/600',
-      1200: '//picsum.photos/500/375',
-    },
-    Portrait: {
-      default: '//picsum.photos/150/200',
-      500: '//picsum.photos/300/400',
-      750: '//picsum.photos/450/600',
-      1200: '//picsum.photos/600/800',
-    },
-    Square: {
-      default: '//picsum.photos/100/100',
-      500: '//picsum.photos/200/200',
-      750: '//picsum.photos/300/300',
-      1200: '//picsum.photos/500/500',
-    },
-  },
-  imageAlt: 'Placeholder image',
-}
 
 const imageModes = ['Landscape', 'Portrait', 'Square']
 
@@ -51,12 +22,12 @@ storiesOf('molecules/blocks/ContentBlock', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const title = text('Title', defaults.title, propsTab)
-    const description = text('Description', defaults.description, propsTab)
-    const category = text('Category', defaults.category, propsTab)
-    const showCta = boolean('Show CTA', true, ctaTab)
-    const cta = text('CTA', defaults.cta, ctaTab)
-    const url = text('URL', defaults.url, ctaTab)
+    const title = text('Title *', data.title, propsTab)
+    const description = text('Description', data.description, propsTab)
+    const category = text('Category', data.category, propsTab)
+    const showCta = boolean('Show Call to Action', true, ctaTab)
+    const cta = text('Call to Action Text', data.cta, ctaTab)
+    const url = text('Call to Action URL', data.url, ctaTab)
     return (
       <ContentBlock
         title={title}
@@ -69,26 +40,27 @@ storiesOf('molecules/blocks/ContentBlock', module)
   })
 
   .addWithJSX('Show more', () => {
-    const title = text('Title', defaults.title, propsTab)
-    const description = text('Description', defaults.description, propsTab)
-    const more = text('Extra text (more)', defaults.more, propsTab)
-    const url = text('URL', defaults.url, propsTab)
-    const category = text('Category', defaults.category, propsTab)
+    const title = text('Title *', data.title, propsTab)
+    const description = text('Description', data.description, propsTab)
+    const more = text('Extra text (more)', data.more, propsTab)
+    const url = text('URL', data.url, propsTab)
+    const category = text('Category', data.category, propsTab)
+    const showImage = boolean('Show Image', true, imageTab)
     const imageMode = select('Image Mode', imageModes, 'Landscape', imageTab)
     const imageSrcSet = object(
       'Image SrcSet',
-      defaults.imageSrcSet[imageMode],
+      data.imageSrcSet[imageMode],
       imageTab
     )
-    const imageAlt = text('Image Alt', defaults.imageAlt, imageTab)
+    const imageAlt = text('Image Alt', data.imageAlt, imageTab)
     return (
       <ContentBlock
         title={title}
         category={category}
         description={description}
         more={more}
-        imageSrcSet={imageSrcSet}
-        imageAlt={imageAlt}
+        imageSrcSet={showImage ? imageSrcSet : null}
+        imageAlt={showImage ? imageAlt : null}
         url={url}
       />
     )
