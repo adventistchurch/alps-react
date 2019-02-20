6595ff7a5e5296d83aa0497b470668369dd57cca
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-20 09:25:44 +0100

    Decouples test data in Figcaption stories.

--- src/molecules/media/Figcaption.stories.js
+++ src/molecules/media/Figcaption.stories.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { text, withKnobs } from '@storybook/addon-knobs'
 
 import Figcaption from './Figcaption'
 
+import data from './Figcaption.stories.json'
+
 const propsTab = 'Props'
-const defaults = {
-  caption: 'Proin dictum lobortis luctus.',
-}
 
 storiesOf('molecules/media/Figcaption', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const caption = text('Caption', defaults.caption, propsTab)
+    const caption = text('Caption *', data.caption, propsTab)
     return <Figcaption>{caption}</Figcaption>
   })
