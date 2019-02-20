795a9a9c9ddea497a8d4230c148b5ad38e58fb2d
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-20 11:25:53 +0100

    Adds support for closing FooterModal with useToggle.

--- src/molecules/components/FooterModal.js
+++ src/molecules/components/FooterModal.js
@@ -2,14 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Icon from '../../atoms/icons/Icon'
+import useToggle from '../../helpers/useToggle'
 
 function FooterModal({ children, text }) {
+  const { onToggle, openClass } = useToggle(true, '', 'this-is-active')
+
   return (
-    <div className="c-modal u-theme--background-color-trans--base u-color--white u-padding text">
+    <div
+      className={`c-modal u-theme--background-color-trans--base u-color--white u-padding text ${openClass}`}
+    >
       <p className="c-modal__body u-font--primary">
         <strong>{children || text}</strong>
       </p>
-      <button className="c-modal__close u-icon u-icon--m u-path-fill--white js-toggle-parent">
+      <button
+        className="c-modal__close u-icon u-icon--m u-path-fill--white js-toggle-parent"
+        onClick={onToggle}
+      >
         <Icon name="close" color="white" />
       </button>
     </div>
