1867c71816870678d978b02bef4c55567c947042
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-20 11:12:52 +0100

    Updates Byline as function. Decouples test data from stories.

--- src/molecules/text/Byline.js
+++ src/molecules/text/Byline.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Byline = ({ children }) => (
-  <div className="byline u-font--secondary--s u-color--gray can-be--white">
-    {children}
-  </div>
-)
-
+function Byline({ children }) {
+  return (
+    <div className="byline u-font--secondary--s u-color--gray can-be--white">
+      {children}
+    </div>
+  )
+}
 Byline.propTypes = {
   children: PropTypes.string,
 }
--- src/molecules/text/Byline.stories.js
+++ src/molecules/text/Byline.stories.js
@@ -4,15 +4,14 @@ import { text, withKnobs } from '@storybook/addon-knobs'
 
 import Byline from './Byline'
 
+import data from './Byline.stories.json'
+
 const propsTab = 'Props'
-const defaults = {
-  textValue: 'Michael Smith',
-}
 
 storiesOf('molecules/text/Byline', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const textValue = text('Byline', defaults.textValue, propsTab)
+    const textValue = text('Byline *', data.textValue, propsTab)
     return <Byline>{textValue}</Byline>
   })
