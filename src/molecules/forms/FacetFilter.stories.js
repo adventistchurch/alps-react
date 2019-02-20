38555fd96aafa4aa1b96601a5a05bcf6dae395e9
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-20 16:14:51 +0100

    Refactors FacetFilter and its stories.

--- src/molecules/forms/FacetFilter.stories.js
+++ src/molecules/forms/FacetFilter.stories.js
@@ -1,171 +1,43 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { array, text, withKnobs } from '@storybook/addon-knobs'
+import { boolean, object, text, withKnobs } from '@storybook/addon-knobs'
 
 import FacetFilter from './FacetFilter'
 
-const propsTab = 'Props'
+import data from './FacetFilter.stories.json'
+
 const currentDate = new Date()
-const currentYear = currentDate.getFullYear()
-const defaults = {
-  action: '/doSomething',
-  title: 'Filter the results',
-  facets: [
+const yearFacet = {
+  name: 'years',
+  options: [
     {
-      name: 'years',
-      options: [
-        {
-          text: 'All years',
-          value: 0,
-        },
-        {
-          text: currentYear,
-          value: currentYear,
-        },
-        {
-          text: currentYear - 1,
-          value: currentYear - 1,
-        },
-        {
-          text: currentYear - 2,
-          value: currentYear - 2,
-        },
-        {
-          text: currentYear - 3,
-          value: currentYear - 3,
-        },
-        {
-          text: currentYear - 4,
-          value: currentYear - 4,
-        },
-        {
-          text: currentYear - 5,
-          value: currentYear - 5,
-        },
-      ],
-      defaultValue: 0,
+      text: 'All years',
+      value: 0,
     },
-    {
-      name: 'months',
-      options: [
-        {
-          text: 'All months',
-          value: 1,
-        },
-        {
-          text: 'January',
-          value: 1,
-        },
-        {
-          text: 'February',
-          value: 2,
-        },
-        {
-          text: 'March',
-          value: 3,
-        },
-        {
-          text: 'April',
-          value: 4,
-        },
-        {
-          text: 'Mai',
-          value: 5,
-        },
-        {
-          text: 'June',
-          value: 6,
-        },
-        {
-          text: 'July',
-          value: 7,
-        },
-        {
-          text: 'August',
-          value: 8,
-        },
-        {
-          text: 'September',
-          value: 9,
-        },
-        {
-          text: 'October',
-          value: 10,
-        },
-        {
-          text: 'November',
-          value: 11,
-        },
-        {
-          text: 'December',
-          value: 12,
-        },
-      ],
-      defaultValue: 0,
-    },
-    {
-      name: 'colors',
-      options: [
-        {
-          text: 'All colors',
-          value: 'all',
-        },
-        {
-          text: 'Red',
-          value: 'red',
-        },
-        {
-          text: 'Green',
-          value: 'green',
-        },
-        {
-          text: 'Blue',
-          value: 'blue',
-        },
-        {
-          text: 'Yellow',
-          value: 'yellow',
-        },
-      ],
-      defaultValue: 'red',
-    },
-    {
-      name: 'countries',
-      options: [
-        {
-          text: 'All countries',
-          value: 'all',
-        },
-        {
-          text: 'United States',
-          value: 'us',
-        },
-        {
-          text: 'Germany',
-          value: 'de',
-        },
-        {
-          text: 'Switzerland',
-          value: 'ch',
-        },
-        {
-          text: 'Norway',
-          value: 'no',
-        },
-      ],
-      defaultValue: 'ch',
-    },
+    ...Array.from(Array(6), (x, index) => {
+      const year = currentDate.getFullYear() - index
+      return { text: year, value: year }
+    }),
   ],
+  defaultValue: 0,
 }
+
+const propsTab = 'Props'
 
 storiesOf('molecules/forms/FacetFilter', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const action = text('Form Action', defaults.action, propsTab)
-    const title = text('Form Title', defaults.title, propsTab)
-    const facets = array('Options', defaults.facets, propsTab)
+    const title = text('Form Title *', data.title, propsTab)
+    const showButton = boolean('Show Button', false, propsTab)
+    const filterLabel = text('Button Label', 'Filter', propsTab)
+    const facets = object('Facets *', [yearFacet, ...data.facets], propsTab)
     return (
-      <FacetFilter action={action} title={title} facets={facets} years months />
+      <FacetFilter
+        title={title}
+        facets={facets}
+        showButton={showButton}
+        filterLabel={filterLabel}
+      />
     )
   })
