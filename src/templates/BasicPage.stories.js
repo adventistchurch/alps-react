36d65461b580265be4f607d78f4c4a84478b3105
Eduardo Ludi <eduludi@gmail.com>
2019-02-21 14:23:34 +0100

    Fixes BasicPage's stories and test data
    - Breadcrumbs prop/tab was missing.
    - Removes Kicker from test data.

--- src/templates/BasicPage.stories.js
+++ src/templates/BasicPage.stories.js
@@ -5,10 +5,11 @@ import { boolean, object, text, withKnobs } from '@storybook/addon-knobs'
 import BasicPage from './BasicPage'
 
 import breakoutData from '../molecules/blocks/BreakoutBlock.stories.json'
+import { breadcrumbsTab } from '../molecules/navigation/Breadcrumbs.stories.js'
 import { asideTab } from '../organisms/asides/Aside.stories.js'
 import { headerTab as globalHeaderTab } from '../organisms/global/Header.stories.js'
 import { footerTab as globalFooterTab } from '../organisms/global/Footer.stories.js'
-import { pageHeaderTab } from '../organisms/sections/PageHeader.stories.js'
+import { pageHeaderTab as headerTab } from '../organisms/sections/PageHeader.stories.js'
 import data from './BasicPage.stories.json'
 
 function getTabData(name, settings = {}) {
@@ -38,10 +39,18 @@ function contentTab(settings = {}) {
   }
 }
 
+function pageHeaderTab(settings = {}) {
+  const { title, url, kicker, tab } = getTabData('Page Header', settings)
+  return headerTab({ title, url, kicker, tab })
+}
+
+function pageBreadcrumbsTab(settings = {}) {
+  const { breadcrumbs, tab } = getTabData('Page Header', settings)
+  return breadcrumbsTab({ items: breadcrumbs, tab })
+}
+
 function globalTab(settings = {}) {
   const { globalHeader, globalFooter, tab } = getTabData('Global', settings)
-
-  console.log(globalHeader)
 
   return {
     header: globalHeaderTab({ ...globalHeader, tab }),
@@ -53,7 +62,8 @@ storiesOf('templates/BasicPage', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const { title, kicker, background, breadcrumbs } = pageHeaderTab()
+    const { title, kicker, background } = pageHeaderTab()
+    const { items: breadcrumbs } = pageBreadcrumbsTab()
     const { contentTitle, contentText } = contentTab()
     const { showSidebar, breakout, aside } = sidebarTab()
     const templateProps = globalTab()
