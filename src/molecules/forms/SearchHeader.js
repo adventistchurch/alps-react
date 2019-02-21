dd48b5d4a2885629f2087fc3d841b811ca85b02b
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-21 21:39:11 +0100

    Uses BaseSearch component in SearchHeader. Decouples test data from stories.

--- src/molecules/forms/SearchHeader.js
+++ src/molecules/forms/SearchHeader.js
@@ -1,107 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Icon from '../../atoms/icons/Icon'
+import BaseSearch from './BaseSearch'
 
-const SearchHeader = ({ title, description, featured, className }) => {
+function SearchHeader({ ...props }) {
   return (
-    <div className="c-page-header__search u-background-color--gray--light can-be--dark-dark u-padding--double--top u-padding--double--bottom">
-      <div className="l-grid l-grid--7-col u-shift--left--1-col--at-large u-spacing--until-medium">
-        <div className="l-grid-item l-grid-item--m--3-col">
-          <input
-            type="search"
-            name="s"
-            className="u-font--secondary--s u-theme--color--darker o-input__search"
-            value="Search..."
-          />
-        </div>
-        <div className="l-grid-item l-grid-item--m--3-col">
-          <div className="u-flex">
-            <button className="c-filter__button o-button u-space--right">
-              <span className="u-icon u-icon--xs u-path-fill--white">
-                <Icon name="search" />
-              </span>
-              Search
-            </button>
-            <span
-              className="c-filter__toggle js-toggle o-button o-button--simple"
-              data-toggled="c-filter"
-              data-prefix="c-filter"
-            >
-              <span className="u-icon u-icon--xs">
-                <Icon name="settings" />
-              </span>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="c-filter">
-        <form className="c-filter__form u-padding--top">
-          <div className="l-grid l-grid--7-col u-shift--left--1-col--at-large u-spacing--until-medium">
-            <div className="c-filter__form-item u-spacing--half l-grid-item l-grid-item--s--2-col l-grid-item--l--1-col">
-              <h3 className="u-font--secondary--s u-font-weight--bold u-text-transform--upper u-color--gray can-be--lighter">
-                Media
-              </h3>
-              {/* {% if search_filter.radio %}
-            {% for item in search_filter.radio %}
-              <div className="c-filter__form-group">
-                <input id="radio-{{ item.id }}" type="radio" name="radio" value="{{ item.label }}" />
-                <label for="radio-{{ item.id }}">{{ item.label }}</label>
-              </div>
-            {% endfor %}
-          {% endif %} */}
-            </div>
-            <div className="c-filter__form-item u-spacing--half l-grid-item l-grid-item--s--2-col l-grid-item--l--1-col">
-              <h3 className="u-font--secondary--s u-font-weight--bold u-text-transform--upper u-color--gray can-be--lighter">
-                Category
-              </h3>
-              {/* {% if search_filter.checkbox %}
-            {% for item in search_filter.checkbox %}
-              <div className="c-filter__form-group">
-                <input id="checkbox-{{ item.id }}" type="checkbox" name="checkbox" value="{{ item.label }}" />
-                <label for="checkbox-{{ item.id }}">{{ item.label }}</label>
-              </div>
-            {% endfor %}
-          {% endif %} */}
-            </div>
-            <div className="c-filter__form-item u-spacing--half l-grid-item l-grid-item--s--2-col l-grid-item--l--1-col">
-              <h3 className="u-font--secondary--s u-font-weight--bold u-text-transform--upper u-color--gray can-be--lighter">
-                Category
-              </h3>
-              {/* {% if search_filter.radio %}
-            {% for item in search_filter.radio %}
-              <div className="c-filter__form-group">
-                <input id="radio-{{ item.id }}" type="radio" name="radio" value="{{ item.label }}" />
-                <label for="radio-{{ item.id }}">{{ item.label }}</label>
-              </div>
-            {% endfor %}
-          {% endif %} */}
-            </div>
-            <div className="l-grid-item--7-col u-space--after-medium">
-              <button className="o-button o-button--outline">
-                <span className="u-icon u-icon--xs">
-                  <Icon name="search" />
-                </span>
-                Search Again
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+    <BaseSearch
+      className="u-padding--double--top u-padding--double--bottom"
+      contentWrapperClass="l-grid l-grid--7-col u-shift--left--1-col--at-large u-spacing--until-medium"
+      contentWrapperNestedClass="l-grid-item l-grid-item--m--3-col"
+      {...props}
+    />
   )
 }
 
 SearchHeader.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string,
-  className: PropTypes.string,
-  featured: PropTypes.bool,
+  filters: PropTypes.array,
+  placeholder: PropTypes.string,
+  searchAgainLabel: PropTypes.string,
+  searchLabel: PropTypes.string,
+  showSearchAgain: PropTypes.bool,
 }
-
 SearchHeader.defaultProps = {
-  className: '',
-  featured: false,
+  filters: [],
+  placeholder: 'Search...',
+  searchAgainLabel: 'Search Again',
+  searchLabel: 'Search',
+  showSearchAgain: true,
 }
 
 export default SearchHeader
--- src/molecules/forms/SearchHeader.stories.js
+++ src/molecules/forms/SearchHeader.stories.js
@@ -1,39 +1,43 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { boolean, object, text, withKnobs } from '@storybook/addon-knobs'
+import { object, text, withKnobs, boolean } from '@storybook/addon-knobs'
 
 import SearchHeader from './SearchHeader'
 
+import data from './SearchHeader.stories.json'
+
 const propsTab = 'Props'
-const defaults = {
-  title: 'Lorem Ipsum',
-  description: 'Fusce nec urna ut tellus accumsan fermentum.',
-  imageSrcSet: {
-    default: '//picsum.photos/500/282',
-    500: '//picsum.photos/900/507',
-    750: '//picsum.photos/700/395',
-    1200: '//picsum.photos/500/282',
-  },
-  imageAlt: 'Placeholder image',
-  isLazy: false,
-}
+const filtersTab = 'Filters'
 
 storiesOf('molecules/forms/SearchHeader', module)
   .addDecorator(withKnobs)
 
   .addWithJSX('Default', () => {
-    const title = text('Title', defaults.title, propsTab)
-    const description = text('Description', defaults.description, propsTab)
-    const imageSrcSet = object('Image SrcSet', defaults.imageSrcSet, propsTab)
-    const imageAlt = text('Image Alt', defaults.imageAlt, propsTab)
-    const isLazy = boolean('Lazy load image', false, propsTab)
+    const placeholder = text('Placeholder', data.placeholder, propsTab)
+    const searchLabel = text('Search Button', data.searchLabel, propsTab)
+    const settingsLabel = text(
+      'Settings Button',
+      'n/a (the text is set in the css)',
+      propsTab
+    )
+    const showSearchAgain = boolean(
+      'Show Search Again Button',
+      true,
+      filtersTab
+    )
+    const searchAgainLabel = text(
+      'Search Again Button',
+      data.searchAgainLabel,
+      propsTab
+    )
+    const filters = object('Filters', data.filters, filtersTab)
     return (
       <SearchHeader
-        title={title}
-        description={description}
-        imageSrcSet={imageSrcSet}
-        imageAlt={imageAlt}
-        isLazy={isLazy}
+        filters={filters}
+        placeholder={placeholder}
+        searchAgainLabel={searchAgainLabel}
+        searchLabel={searchLabel}
+        showSearchAgain={showSearchAgain}
       />
     )
   })
