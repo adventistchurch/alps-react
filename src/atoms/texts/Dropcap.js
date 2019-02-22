83c27806a86d7923922dd33757cce7f0c80237b0
Eduardo Ludi <eduludi@gmail.com>
2019-02-22 10:41:05 +0100

    Updates Dropcap with a Fragment. Uses hidden attr instead of CSS styles.

--- src/atoms/texts/Dropcap.js
+++ src/atoms/texts/Dropcap.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const hideLetter = {
-  display: 'none',
-}
-
 function Dropcap({ letter }) {
   return (
-    <span
-      className="o-dropcap u-theme--background-color--base"
-      data-letter={letter}
-    >
-      <span style={hideLetter}>{letter}</span>
-    </span>
+    <>
+      <span
+        className="o-dropcap u-theme--background-color--base"
+        data-letter={letter}
+      />
+      <span hidden>{letter}</span>
+    </>
   )
 }
 
