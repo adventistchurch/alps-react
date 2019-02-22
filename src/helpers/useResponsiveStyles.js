c351b10c64af8e265006027105d56c60ea046b42
Eduardo Ludi <eduludi@gmail.com>
2019-02-22 12:06:50 +0100

    Fixes bug with useResponsiveStyles.
    Allows passing some settings like `mediaFeature` and `unit` to it.

--- src/helpers/useResponsiveStyles.js
+++ src/helpers/useResponsiveStyles.js
@@ -1,15 +1,34 @@
-// Given a rule, wraps it in a media query
 /**
- *
+ * Given a rule, wraps it in a media query
  * @param {string} rule - A CSS rule that can be wrapped
  * @param {string} query - A media query expressions (`min-width`, `max-with`, etc.)
  */
 function wrapMediaQuery(rule, query) {
-  return `
-    ${query ? `@media(${query}) {` : ''}
-      ${rule}
-    ${query ? '}' : ''}
-    `
+  return query ? `\n@media(${query}) {\t${rule}\n}` : rule
+}
+
+/**
+ * Makes sure default querie is at the begining.
+ *
+ * Example:
+ * - Input keys:  ["500", "750", "1200", "default"]
+ * - Output keys: ["default", "500", "750", "1200"]
+ *
+ * @param {Map} data
+ */
+function sortQueries(data) {
+  const queries = []
+  const { default: defaultQuery, ...others } = data
+
+  if (defaultQuery) queries.push('default')
+  queries.push(...Object.keys(others))
+  return queries
+}
+
+// Define some default settings
+const defaults = {
+  mediaFeature: 'min-width',
+  unit: 'px',
 }
 
 /**
@@ -17,18 +36,17 @@ function wrapMediaQuery(rule, query) {
  *
  * @param {function} rule - Function that returns a CSS rule as string
  * @param {Map} data - Map with data that will be used by `rule()` for a specific media query
- * @param {Map} mediaQueries - Map of media queries to be looped through
+ * @param {String} mediaFeature - default media features
  */
-function useResponsiveStyles(rule, data = {}) {
-  const styles = Object.keys(data)
-    .reverse()
-    .map(source => {
-      const size = source === 'default' ? null : source
-      return wrapMediaQuery(
-        rule(data[source]),
-        size ? `min-width: ${size}px` : null
-      )
-    })
+function useResponsiveStyles(rule, data = {}, settings = defaults) {
+  const { mediaFeature, unit } = settings
+  const styles = sortQueries(data).map(source => {
+    const size = source === 'default' ? null : source
+    return wrapMediaQuery(
+      rule(data[source]),
+      size ? `${mediaFeature}: ${size}${unit}` : null
+    )
+  })
 
   return styles.join('')
 }
