faf6f2d62ce62779a55ebe98bb00bd7be5fd6695
Eduardo Ludi <eduludi@gmail.com>
2019-02-23 21:43:39 +0100

    Fixes some broken paths

--- src/atoms/images/Logo.js
+++ src/atoms/images/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import logos, { logoNames } from './logos'
+import logos, { logoNames } from './logos/index'
 import { svgFillColors } from '../global/colors'
 
 function Logo({ name, ...props }) {
--- src/molecules/index.js
+++ src/molecules/index.js
@@ -1,19 +1,28 @@
+// MOLECULES:
+
+// - Blocks
 export { default as BreakoutBlock } from './blocks/BreakoutBlock'
 export { default as ContentBlock } from './blocks/ContentBlock'
 export { default as ContentBlockExpand } from './blocks/ContentBlockExpand'
 export { default as GalleryBlock } from './blocks/GalleryBlock'
-export { default as MediaBlock } from './blocks/MediaBlock/index'
+export { default as MediaBlock } from './blocks/MediaBlock'
+
+// - Components
 export { default as Accordion } from './components/Accordion'
 export { default as Carousel } from './components/Carousel'
 export { default as Comment } from './components/Comment'
 export { default as ShareTools } from './components/ShareTools'
 export { default as Slide } from './components/Slide'
+
+// - Forms
 export { default as CommentForm } from './forms/CommentForm'
 export { default as FacetFilter } from './forms/FacetFilter'
 export { default as InlineForm } from './forms/InlineForm'
 export { default as LoginForm } from './forms/LoginForm'
 export { default as Search } from './forms/Search'
 export { default as SearchHeader } from './forms/SearchHeader'
+
+// - Media:
 export { default as BreakoutImage } from './media/BreakoutImage'
 export { default as Figcaption } from './media/Figcaption'
 export { default as Figure } from './media/Figure'
@@ -21,9 +30,23 @@ export { default as FigureWithCaption } from './media/FigureWithCaption'
 export {
   default as VideoFigureWithCaption,
 } from './media/VideoFigureWithCaption'
-export { default as ArticleNavigation } from './navigation/ArticleNavigation'
-export { default as AsideNavigation } from './navigation/AsideNavigation'
+
+// - Navigation:
 export { default as Breadcrumbs } from './navigation/Breadcrumbs'
+export { default as DrawerNavigation } from './navigation/DrawerNavigation'
+export {
+  default as FooterPrimaryNavigation,
+} from './navigation/FooterPrimaryNavigation'
+export {
+  default as FooterSecondaryNavigation,
+} from './navigation/FooterSecondaryNavigation'
+export { default as PrimaryNavigation } from './navigation/PrimaryNavigation'
+export {
+  default as SecondaryNavigation,
+} from './navigation/SecondaryNavigation'
+export { default as Pagination } from './navigation/Pagination'
+
+// - Text:
 export { default as Byline } from './text/Byline'
 export { default as ExtendedQuote } from './text/ExtendedQuote'
 export { default as Highlight } from './text/Highlight'
