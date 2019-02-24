8f0fc07d6329dd0fed4a8c556159d518f926ac08
Eduardo Ludi <eduludi@gmail.com>
2019-02-24 20:32:48 +0100

    Adds link to HeadingBlock

--- src/molecules/blocks/HeadingBlock.js
+++ src/molecules/blocks/HeadingBlock.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function HeadingBlock({ title }) {
+function HeadingBlock({ title, linkText, url }) {
   return (
     <div className="c-block__heading u-theme--border-color--darker">
       <h3 className="c-block__heading-title u-theme--color--darker">{title}</h3>
+      {url && linkText && (
+        <a
+          href={url}
+          className="c-block__heading-link u-theme--color--base u-theme--link-hover--dark"
+        >
+          {linkText}
+        </a>
+      )}
     </div>
   )
 }
 
 HeadingBlock.propTypes = {
+  linkText: PropTypes.string,
   title: PropTypes.string.isRequired,
+  url: PropTypes.string,
 }
 
 export default HeadingBlock
