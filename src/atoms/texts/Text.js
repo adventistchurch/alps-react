4fac500858c17a20a8143fcd6ffca9499254bf40
Eduardo Ludi <eduludi@gmail.com>
2019-02-24 15:44:26 +0100

    Adds prop 'as' to Text so it can be rendered as a different element.

--- src/atoms/texts/Text.js
+++ src/atoms/texts/Text.js
@@ -5,7 +5,14 @@ import useDropcap from '../../helpers/useDropcap'
 import getSpacing, { spacingSides, spacingSizes } from '../global/spacing'
 import Dropcap from './Dropcap'
 
-function Text({ children, hasDropcap, hasSpacing, spacingSize, spacingSide }) {
+function Text({
+  as,
+  children,
+  hasDropcap,
+  hasSpacing,
+  spacingSize,
+  spacingSide,
+}) {
   const content = useDropcap(children, { Dropcap, enabled: hasDropcap })
   const dropcap = hasDropcap ? 'has-dropcap' : ''
   const spacing = hasSpacing
@@ -15,15 +22,23 @@ function Text({ children, hasDropcap, hasSpacing, spacingSize, spacingSide }) {
       })
     : ''
 
-  return <div className={`text ${spacing} ${dropcap}`}>{content}</div>
+  return React.createElement(
+    as,
+    { className: `text ${spacing} ${dropcap}` },
+    content
+  )
 }
 
 Text.propTypes = {
+  as: PropTypes.oneOf(['article', 'div', 'section']),
   children: PropTypes.node,
   hasDropcap: PropTypes.bool,
   hasSpacing: PropTypes.bool,
   spacingSide: PropTypes.oneOf(spacingSides),
   spacingSize: PropTypes.oneOf(spacingSizes),
+}
+Text.defaultProps = {
+  as: 'div',
 }
 
 export default Text
