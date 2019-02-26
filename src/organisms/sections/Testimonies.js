eff71753ac4aa812226f87f83bf7e8d0eeed68b1
Eduardo Ludi <eduludi@gmail.com>
2019-02-26 17:58:20 +0100

    Sets Testimonies to work with Slider.

--- src/organisms/sections/Testimonies.js
+++ src/organisms/sections/Testimonies.js
@@ -2,9 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Button from '../../atoms/buttons/Button'
-import renderItems from '../../helpers/renderItems'
 import ContentBlock from '../../molecules/blocks/ContentBlock'
 import HeadingBlock from '../../molecules/blocks/HeadingBlock'
+
+import Slider from '../../helpers/Slider'
+
+const sliderProps = {
+  fade: false,
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: {
+    '500': {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    },
+    '900': {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    },
+    '1300': {
+      slidesToShow: 6,
+      slidesToScroll: 6,
+    },
+  },
+}
 
 function Testimonies({ items, linkText, linkUrl, shareText, shareUrl, title }) {
   return (
@@ -16,20 +38,24 @@ function Testimonies({ items, linkText, linkUrl, shareText, shareUrl, title }) {
         title={title}
         url={linkUrl}
       />
-
-      <div className="l-grid l-grid-wrap l-grid-wrap--6-of-7">
-        {/* TODO: replace js-carousel__multi-item" */}
-        {renderItems(items, item => (
-          <div className="l-grid-item--6-col l-grid-item--m--3-col l-grid-item--l--2-col l-grid-item--xl--1-col u-space--bottom u-padding--zero--sides">
-            {/*
-             * TODO: ContentBlock should be able to set the following classes:
-             * - block: "u-spacing--half u-padding--right"
-             * - title: "u-font--secondary--m u-text-transform--upper"
-             */}
-            <ContentBlock {...item} />
+      <Slider
+        className="l-grid l-grid-wrap l-grid-wrap--6-of-7"
+        {...sliderProps}
+      >
+        {items.map((item, key) => (
+          <div
+            className="l-grid-item--6-col l-grid-item--m--3-col l-grid-item--l--2-col l-grid-item--xl--1-col u-space--bottom u-padding--zero--sides"
+            key={`slide-${key}`}
+          >
+            <ContentBlock
+              {...item}
+              className="u-spacing--half u-padding--right"
+              titleClass="u-font--secondary--m u-text-transform--upper"
+              titleStrong
+            />
           </div>
         ))}
-      </div>
+      </Slider>
       <Button className="u-space--lef" text={shareText} url={shareUrl} />
     </section>
   )
