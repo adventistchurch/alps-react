4dcce0cf5c9084ce34b5e8bbfae274497391c4de
Eduardo Ludi <eduludi@gmail.com>
2019-02-27 14:58:10 +0100

    Improves Carousel stories.

--- src/molecules/components/Carousel.stories.js
+++ src/molecules/components/Carousel.stories.js
@@ -8,11 +8,64 @@ import data from './Carousel.stories.json'
 
 const propsTab = 'Props'
 
+function getTabData(name, settings = {}) {
+  return {
+    tab: name,
+    ...Carousel.defaultProps,
+    ...data,
+    ...settings,
+  }
+}
+
+function optionsTab(settings = {}) {
+  const { showArrows, showDots, tab } = getTabData('Options', {
+    settings,
+  })
+
+  return {
+    showArrows: boolean('Show Arrows', showArrows, tab),
+    showDots: boolean('Show Dots', showDots, tab),
+  }
+}
+
+function slidesTab(settings = {}) {
+  const { slides, tab } = getTabData('Slides', {
+    settings,
+  })
+
+  return {
+    slides: object('Slides *', slides, tab),
+  }
+}
+
+export function carouselTab(settings = {}) {
+  const props = getTabData('Carousel', {
+    settings,
+  })
+
+  return {
+    ...slidesTab(props),
+    ...optionsTab(props),
+  }
+}
+
 storiesOf('molecules/components/Carousel', module)
   .addDecorator(withKnobs)
 
-  .addWithJSX('Default', () => {
-    const showArrows = boolean('Show Arrows', true, propsTab)
-    const slides = object('Slides *', data.slides, propsTab)
-    return <Carousel slides={slides} showArrows={showArrows} />
+  .addWithJSX('Default (with dots)', () => {
+    const { showArrows, showDots } = optionsTab()
+    const { slides } = slidesTab()
+
+    return (
+      <Carousel slides={slides} showArrows={showArrows} showDots={showDots} />
+    )
+  })
+
+  .addWithJSX('With arrows', () => {
+    const { showArrows, showDots } = optionsTab()
+    const { slides } = slidesTab()
+
+    return (
+      <Carousel slides={slides} showArrows={showArrows} showDots={showDots} />
+    )
   })
