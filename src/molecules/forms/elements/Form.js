2d025a44997d09c459e90dbc632b142ce1ef1e45
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-27 12:24:49 +0100

    Refactors Form using Element component.

--- src/molecules/forms/elements/Form.js
+++ src/molecules/forms/elements/Form.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Form({ spacing, children, className, inline, ...props }) {
-  const formClass = `${spacing ? `u-spacing--${spacing}` : ''} ${
-    inline ? 'c-form--inline' : ''
-  } ${className}`
+import Element from '../../../helpers/Element'
+import { darkThemeClass } from '../../../atoms/global/colors'
+
+function Form({ children, className, darkMode, inline, ...props }) {
+  const classes = []
+
+  classes.push('c-form')
+  if (inline) classes.push('c-form--inline')
+  if (darkMode) classes.push(darkThemeClass)
+  if (className) classes.push(className)
+
+  const formClass = classes.join(' ')
+
   return (
-    <form className={formClass} {...props}>
+    <Element
+      as="form"
+      className={formClass}
+      themeBackground={darkMode ? 'darker' : null}
+      {...props}
+    >
       {children}
-    </form>
+    </Element>
   )
 }
 
@@ -16,15 +30,14 @@ Form.propTypes = {
   action: PropTypes.string,
   children: PropTypes.node,
   className: PropTypes.string,
+  darkMode: PropTypes.bool,
   inline: PropTypes.bool,
   method: PropTypes.oneOf(['post', 'get']),
   noValidate: PropTypes.bool,
   onSubmit: PropTypes.func,
   role: PropTypes.oneOf(['search']),
-  spacing: PropTypes.oneOf(['half']),
+  ...Element.propTypes,
 }
-Form.defaultProps = {
-  className: '',
-}
+Form.defaultProps = {}
 
 export default Form
