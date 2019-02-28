b2d8ea191a01bd2586e18489975d426b7e1cd27a
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-28 22:10:18 +0100

    Refactors Search.

--- src/molecules/forms/Search.js
+++ src/molecules/forms/Search.js
@@ -3,25 +3,29 @@ import PropTypes from 'prop-types'
 
 import Fieldset from './elements/Fieldset'
 import Form from './elements/Form'
+import SearchField from './elements/SearchField'
 
 import useInputFocus from '../../helpers/useInputFocus'
 
 function Search({ submitLabel, hasFocus, placeholder, title, ...props }) {
   const inputFocusRef = useInputFocus(hasFocus)
 
+  // Remove from the flow but leave available to screen readers
+  const screenReaderClass = 'is-vishidden'
+
   return (
     <Form className="search-form" role="search" {...props}>
-      <Fieldset legend={title} legendClass="is-vishidden">
-        <input
-          className="search-form__input u-font--secondary--s"
+      <Fieldset legend={title} legendClass={screenReaderClass}>
+        <SearchField
+          className="search-form__input"
           name="search"
-          placeholder={placeholder ? `${placeholder}` : null}
+          placeholder={placeholder}
           ref={inputFocusRef}
           required
-          type="search"
         />
-        <button className="search-form__submit is-vishidden">
-          <span className="is-vishidden">{submitLabel}</span>
+        {/* TODO: create component for screen readers? */}
+        <button className={`search-form__submit ${screenReaderClass}`}>
+          <span className={screenReaderClass}>{submitLabel}</span>
         </button>
       </Fieldset>
     </Form>
@@ -39,7 +43,6 @@ Search.propTypes = {
 Search.defaultProps = {
   submitLabel: 'Search',
   title: 'Search',
-  placeholder: 'Search',
 }
 
 export default Search
