65bf9b32c7cc8849c517247b02968b45f464652a
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-02-28 22:12:07 +0100

    Refactors FacetFilter.

--- src/molecules/forms/FacetFilter.js
+++ src/molecules/forms/FacetFilter.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Element from '../../helpers/Element'
 import Form from './elements/Form'
 import renderItems from '../../helpers/renderItems'
 import Fieldset from './elements/Fieldset'
 import Facet from './elements/Facet'
 
-function FacetFilter({ facets, filterLabel, showButton, title, ...props }) {
-  const buttonClass = showButton ? '' : 'is-vishidden'
+function FacetFilter({ facets, filterLabel, title, ...props }) {
+  // Remove from the flow but leave available to screen readers
+  const screenReaderClass = 'is-vishidden'
+
   return (
     <Form className="c-facet-filter-form" {...props}>
-      <Fieldset
-        legend={title}
-        legendClass="u-font--secondary--m u-theme--color--darker"
-        spacing="half"
-      >
-        <div className="u-spacing">
+      <Fieldset legend={title} spacing="half">
+        <Element spacing>
           {renderItems(facets, Facet)}
-          <button className={`search-form__submit ${buttonClass}`}>
-            <span className={buttonClass}>{filterLabel}</span>
+          {/* TODO: create component for screen readers? */}
+          <button className={`search-form__submit ${screenReaderClass}`}>
+            <span className={screenReaderClass}>{filterLabel}</span>
           </button>
-        </div>
+        </Element>
       </Fieldset>
     </Form>
   )
@@ -33,6 +33,7 @@ FacetFilter.propTypes = {
   title: PropTypes.string,
 }
 FacetFilter.defaultProps = {
+  title: 'Filter the results',
   facets: [],
   filterLabel: 'Filter',
 }
