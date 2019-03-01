2d754ed9e807c261620b4c82290291c0756b44d3
Eduardo Ludi <eduludi@gmail.com>
2019-03-01 13:08:41 +0100

    Refactors DrawerNavigation with reusable components.

--- src/molecules/navigation/DrawerNavigation.js
+++ src/molecules/navigation/DrawerNavigation.js
@@ -2,8 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import IconWrap from '../../atoms/icons/IconWrap'
+import GridSeven from '../../atoms/grids/GridSeven'
+import GridItem from '../../atoms/grids/GridItem'
 import SearchForm from '../forms/Search'
 import useDrawerContext from '../../helpers/useDrawerContext'
+import Element from '../../helpers/Element'
 import PrimaryNavigation from './PrimaryNavigation'
 import SecondaryNavigation from './SecondaryNavigation'
 
@@ -17,30 +20,39 @@ function DrawerNavigation({
   const { closeDrawer, isOpen } = useDrawerContext()
 
   return (
-    <div
-      className={`c-drawer l-grid l-grid--7-col ${
-        isOpen.menu ? 'this-is-active' : ''
-      }`}
-    >
-      <div
-        className="c-drawer__toggle js-toggle-parent u-theme--background-color-trans--darker"
+    <GridSeven className={`c-drawer ${isOpen.menu ? 'this-is-active' : ''}`}>
+      <Element
+        className="c-drawer__toggle"
+        themeBackgroundTrans="darker"
         onClick={closeDrawer}
       >
+        {/* TODO: Can this be a SVG icon? */}
         <div className="u-icon o-icon__close">
           <span />
           <span />
         </div>
-      </div>
-      <div className="l-grid-wrap--6-of-7 l-grid-item--s--6-col c-drawer__container u-spacing u-theme--background-color--darker">
+      </Element>
+      <GridItem
+        className="c-drawer__container l-grid-wrap--6-of-7"
+        wrap="6"
+        sizeAtS="6"
+        noItemClass
+        spacing
+        themeBackground="darker"
+      >
         <div className="c-drawer__search">
           <SearchForm onSearch={onSearch} hasFocus={isOpen.search} />
         </div>
         <div className="c-drawer__nav">
           <div className="c-drawer__nav-primary">
             <PrimaryNavigation {...primaryNav} />
-            <ul className="c-drawer__subnav u-theme--background-color--darker">
-              {/* TODO: What is this for?? */}
-            </ul>
+            <Element
+              as="ul"
+              className="c-drawer__subnav"
+              themeBackground="darker"
+            >
+              {/* TODO: What is this subnav for? Any examples? */}
+            </Element>
           </div>
           <div className="c-drawer__nav-secondary">
             <SecondaryNavigation
@@ -60,8 +72,8 @@ function DrawerNavigation({
             {aboutRight}
           </div>
         </div>
-      </div>
-    </div>
+      </GridItem>
+    </GridSeven>
   )
 }
 
