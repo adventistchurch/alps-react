53e71e1e741440338ce9fe0cf8951ae08b30eda1
Eduardo Ludi <eduludi@gmail.com>
2019-03-01 09:59:14 +0100

    Updates spacing config with commons classes.

--- src/atoms/global/spacing.js
+++ src/atoms/global/spacing.js
@@ -5,32 +5,28 @@
  * - spacing
  */
 
+import { sides as commonSides, getBaseClass } from './commons'
+
 export const sizes = ['quarter', 'half', 'double', 'triple', 'quad', 'zero']
-export const sides = ['top', 'bottom', 'left', 'right', 'sides']
+export const sides = [...commonSides, 'sides']
 export const afterSizes = ['medium', 'large']
 export const untilSizes = ['small', 'medium', 'large', 'xxlarge']
 
-function mod(key) {
-  return key ? `--${key}` : ''
-}
-
-function getBaseClass(baseClass, size, side, extra) {
-  return `${baseClass}${mod(size)}${mod(side)}${mod(extra)}`
-}
+export const clearFixClass = 'u-clear-fix'
 
 export function getPaddingClass({ size, side }) {
-  return getBaseClass('u-padding', size, side)
+  return getBaseClass('u-padding', [size, side])
 }
 
 export function getSpaceClass({ size, side }) {
-  return getBaseClass('u-space', size, side)
+  return getBaseClass('u-space', [size, side])
 }
 
 export function getSpacingClass({ after, size, side, until }) {
-  return getBaseClass(
-    'u-spacing',
+  return getBaseClass('u-spacing', [
     size,
     side,
-    until ? `until-${until}` : after ? `after-${after}` : null
-  )
+    until ? `until-${until}` : '',
+    after ? `after-${after}` : '',
+  ])
 }
