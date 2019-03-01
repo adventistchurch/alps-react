ab968b2dfe1d07cccadba8780fca721f3ac22b27
Olivier Guy <olivier.guy@stimme-der-hoffnung.de>
2019-03-01 08:06:25 +0100

    Deletes Facet and replaces it by Dropdown where used.

--- src/molecules/forms/FacetFilter.js
+++ src/molecules/forms/FacetFilter.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Dropdown from './elements/Dropdown'
 import Element from '../../helpers/Element'
+import Fieldset from './elements/Fieldset'
 import Form from './elements/Form'
 import renderItems from '../../helpers/renderItems'
-import Fieldset from './elements/Fieldset'
-import Facet from './elements/Facet'
 
 function FacetFilter({ facets, filterLabel, title, ...props }) {
   // Remove from the flow but leave available to screen readers
@@ -15,7 +15,7 @@ function FacetFilter({ facets, filterLabel, title, ...props }) {
     <Form className="c-facet-filter-form" {...props}>
       <Fieldset legend={title} spacing="half">
         <Element spacing>
-          {renderItems(facets, Facet)}
+          {renderItems(facets, Dropdown)}
           {/* TODO: create component for screen readers? */}
           <button className={`search-form__submit ${screenReaderClass}`}>
             <span className={screenReaderClass}>{filterLabel}</span>
