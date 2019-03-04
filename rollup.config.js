9ec0aa636738d9af22306cec221466ca487ff6ae
Eduardo Ludi <eduludi@gmail.com>
2019-03-04 14:28:57 +0100

    Updates rollup config to support UMD, CJS and ESM.
    Adds some rollup configs as devDependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,91 @@
-import { terser } from 'rollup-plugin-terser'
+import { readdirSync } from 'fs'
+import path from 'path'
+
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
-// @babel/plugin-proposal-export-default-from
+import resolve from 'rollup-plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
 
-import { name as libraryName } from './package.json'
+import pkg from './package.json'
 
-const babelConf = {
-  babelrc: false,
-  presets: [
-    ['@babel/preset-env', { modules: false }],
-    ['@babel/preset-react', {}],
-  ],
-  exclude: 'node_modules/**',
-}
-
-const outputs = [
-  {
-    fileExt: '.js',
-  },
-  {
-    fileExt: '.min.js',
-    plugins: [terser()],
-  },
+const EXTENSIONS = ['.js', '.json']
+const CODES = [
+  'THIS_IS_UNDEFINED',
+  'MISSING_GLOBAL_NAME',
+  'CIRCULAR_DEPENDENCY',
 ]
 
-export default outputs.map(({ fileExt, plugins = [] }) => ({
-  input: 'src/index.js',
-  output: {
-    file: 'lib/' + libraryName + fileExt,
-    format: 'cjs',
-    sourcemap: true,
+const getChunks = URI =>
+  readdirSync(path.resolve(URI))
+    .filter(x => x.includes('.js') && !x.includes('.stories.js'))
+    .reduce((a, c) => ({ ...a, [c.replace('.js', '')]: `src/${c}` }), {})
+
+const discardWarning = warning => {
+  if (CODES.includes(warning.code)) {
+    return
+  }
+  console.error(warning)
+}
+
+const env = process.env.NODE_ENV
+
+const commonPlugins = () => [
+  external({
+    includeDependencies: false,
+  }),
+  babel({
+    babelrc: false,
+    presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+    extensions: EXTENSIONS,
+    exclude: 'node_modules/**',
+  }),
+  commonjs({
+    include: /node_modules/,
+  }),
+  replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+  resolve({
+    extensions: EXTENSIONS,
+    preferBuiltins: false,
+  }),
+  filesize(),
+]
+
+export default [
+  {
+    onwarn: discardWarning,
+    input: 'src/index.js',
+    output: {
+      esModule: false,
+      file: pkg.unpkg,
+      format: 'umd',
+      name: pkg.name,
+      exports: 'named',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+    },
+    plugins: [...commonPlugins(), env === 'production' && terser()],
   },
-  plugins: [babel(babelConf), ...plugins, filesize()],
-  external: ['react', 'react-dom'],
-}))
+  {
+    onwarn: discardWarning,
+    input: getChunks('src'),
+    output: [
+      {
+        dir: 'build/esm',
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        dir: 'build/cjs',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins: commonPlugins(),
+  },
+]
