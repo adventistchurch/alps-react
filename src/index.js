a9fa47776fb7422f516a50854fa3a72e87d2dfcc
Eduardo Ludi <eduludi@gmail.com>
2019-03-04 14:27:15 +0100

    Exports externalAssets (with extrenal css paths) from src/index

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ export * from './molecules/index'
 export * from './organisms/index'
 export * from './templates/index'
 
+export const externalAssets = {
+  css: [
+    '//fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i|Noto+Serif:400,400i,700,700i',
+    '//cdn.adventist.org/alps/3/latest/css/main.css',
+  ],
+}
+
 export { default as Wrapper } from './Wrapper'
