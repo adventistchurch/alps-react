d5da0d4853bf7c7b12b34a3cf33d2cf93a8fe6ee
Eduardo Ludi <eduludi@gmail.com>
2019-03-06 10:38:49 +0100

    Adds display class and options configuration.

--- src/atoms/global/commons.js
+++ src/atoms/global/commons.js
@@ -2,9 +2,15 @@
  * Custom Configurations & methods
  */
 
+// Common Sides
 export const sides = ['top', 'bottom', 'left', 'right']
 
+// Display
+export const displayClass = 'u-display'
+export const displayOptions = ['block', 'inline-block']
 export const vishiddenClass = 'is-vishidden'
+
+// Common Functions:
 
 function getMod(key) {
   return key ? `--${key}` : ''
