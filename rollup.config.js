fef7170e2a15493da4ca0a8fab848df788b649ba
Eduardo Ludi <eduludi@gmail.com>
2019-11-22 12:01:33 +0100

    Updates rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -136,7 +136,7 @@ export default [
       ...commonPlugins(),
       env === 'production' && terser(),
     ],
-    external: ['react', 'react-dom'],
+    external: ['react', 'react-dom', 'prop-types'],
   },
   // CJS and ESM
   {
