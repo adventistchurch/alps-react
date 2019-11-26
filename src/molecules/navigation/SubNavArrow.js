e5a431cf200c979c7cdb67ca2270faed216d4a2f
Eduardo Ludi <eduludi@gmail.com>
2019-11-26 20:24:21 +0100

    Adds isSubNav prop to SubNavArrow

--- src/molecules/navigation/SubNavArrow.js
+++ src/molecules/navigation/SubNavArrow.js
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types'
 import { svgFillColors } from '../../atoms/global/colors'
 import { Span } from '../../helpers/Element'
 
-function SubNavArrow({ className, fill, onClick }) {
+function SubNavArrow({ className, fill, isSubNav, onClick }) {
   return (
     <Span
-      className={`c-primary-nav__arrow c-subnav__arrow o-arrow--down ${className}`}
+      className={`c-primary-nav${
+        isSubNav ? '__subnav' : ''
+      }__arrow c-subnav__arrow o-arrow--down ${className}`}
       onClick={onClick}
       pathFill={fill}
     />
@@ -18,6 +20,7 @@ SubNavArrow.propTypes = {
   className: PropTypes.string,
   fill: PropTypes.oneOf(svgFillColors),
   onClick: PropTypes.func,
+  isSubNav: PropTypes.bool,
 }
 SubNavArrow.defaultProps = {
   className: '',
