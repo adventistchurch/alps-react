6c19931ffaab11e737233878d2559ff5e113b2f0
Eduardo Ludi <eduludi@gmail.com>
2019-12-15 17:09:04 +0100

    Exports ListContent from organism/content/index

--- src/organisms/content/index.js
+++ src/organisms/content/index.js
@@ -1,4 +1,5 @@
 export { default as ArticleContent } from './ArticleContent'
 export { default as BodyContent } from './BodyContent'
+export { default as ListContent } from './ListContent'
 export { default as PageContent } from './PageContent'
 export { default as SearchResults } from './SearchResults'
