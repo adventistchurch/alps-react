f0f873b747644dc1a9b38986855131237fa38afd
Eduardo Ludi <eduludi@gmail.com>
2020-01-23 14:18:46 +0100

    Improves ListContent component

--- src/organisms/content/ListContent.js
+++ src/organisms/content/ListContent.js
@@ -2,24 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { Div } from '../../helpers/Element'
+import HeadingBlock from '../../molecules/blocks/HeadingBlock'
 import Pagination from '../../molecules/navigation/Pagination'
 
-function ListContent({ children, pagination }) {
+function ListContent({ children, pagination, title, linkUrl, linkLabel }) {
   return (
-    <>
-      {children}
-      {pagination && (
-        <Div space>
-          <Pagination {...pagination} />
-        </Div>
+    <Div className="c-block-wrap" spacingSize="double" paddingSide="right">
+      {title && (
+        <HeadingBlock title={title} linkText={linkLabel} url={linkUrl} />
       )}
-    </>
+      <Div className="c-block-wrap__content" spacingSize="double">
+        {children}
+      </Div>
+      {pagination && <Pagination {...pagination} />}
+    </Div>
   )
 }
 
 ListContent.propTypes = {
   children: PropTypes.node,
+  linkLabel: PropTypes.string,
+  linkUrl: PropTypes.string,
   pagination: PropTypes.object,
+  title: PropTypes.string,
+}
+ListContent.propTypes = {
+  linkLabel: 'See all',
+  linkUrl: '#',
 }
 
 export default ListContent
--- src/organisms/content/ListContent.stories.js
+++ src/organisms/content/ListContent.stories.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { object } from '@storybook/addon-knobs'
+import { object, text } from '@storybook/addon-knobs'
 
-import ContentBlock from '../../molecules/blocks/ContentBlock'
+import MediaBlock from '../../molecules/blocks/MediaBlock'
 import renderItems from '../../helpers/renderItems'
 import { paginationTab } from '../../molecules/navigation/Pagination.stories.js'
 
@@ -18,16 +18,54 @@ function getTabData(name, settings = {}) {
     ...settings,
   }
 }
+export function headingTab(settings = {}) {
+  const { linkLabel, linkUrl, title, tab } = getTabData('Heading', settings)
 
-export function listContentTab(settings = {}) {
-  const { items, tab } = getTabData('Pagination', settings)
+  return {
+    title: text('Title', title, tab),
+    linkLabel: text('Link label', linkLabel, tab),
+    linkUrl: text('Link URL', linkUrl, tab),
+  }
+}
+
+export function itemsTab(settings = {}) {
+  const { items, tab } = getTabData('Items', settings)
   const paginationProps = settings.showPagination
     ? paginationTab(settings.pagination)
     : null
 
   return {
-    children: renderItems(object('Items *', items, tab), ContentBlock),
+    children: renderItems(object('Items *', items, tab), MediaBlock, {
+      stackedUntilSmall: true,
+      asBackgroundImage: false,
+      seven: true,
+      blockProps: {
+        gridBreak: 'large',
+        gridWrap: true,
+        seven: true,
+        spacingUntil: 'small',
+      },
+      imageProps: {
+        gridItemSizeAtS: 2,
+        gridItemSizeAtL: 1,
+        paddingSize: 'zero',
+        paddingSide: 'sides',
+      },
+      contentProps: {
+        gridItemSizeAtS: 4,
+        gridItemSizeAtL: 3,
+        paddingSide: 'left',
+        flexJustify: 'start',
+      },
+    }),
     pagination: paginationProps,
+  }
+}
+
+export function listContentTab(settings = {}) {
+  return {
+    ...headingTab(settings),
+    ...itemsTab(settings),
   }
 }
 
