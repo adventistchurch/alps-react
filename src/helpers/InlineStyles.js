1dcee8857dca713d1ca28a4c3b6bf3347b0625fa
Eduardo Ludi <eduludi@gmail.com>
2020-01-28 09:01:24 +0100

    Fixes issue with SSR in InlineStyles
    Now setting styles with dangerouslySetInnerHTML

--- src/helpers/InlineStyles.js
+++ src/helpers/InlineStyles.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+function setStyle(styles) {
+  return { __html: styles }
+}
+
 function InlineStyles({ styles }) {
-  return <style type="text/css">{styles}</style>
+  return (
+    <style type="text/css" dangerouslySetInnerHTML={setStyle(styles)}></style>
+  )
 }
 
 InlineStyles.propTypes = {
