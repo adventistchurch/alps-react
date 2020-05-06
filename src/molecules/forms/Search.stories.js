5e390570563ef4bb7c30889a69dcacd9a0a02e0f
Eduardo Ludi <eduludi@gmail.com>
2020-05-06 19:13:35 +0200

    Moves knobs out of stories for molecules/forms/Search

--- src/molecules/forms/Search.stories.js
+++ src/molecules/forms/Search.stories.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { text } from '@storybook/addon-knobs'
 
 import Search from './Search'
 
-import data from './Search.stories.json'
-
-const propsTab = 'Props'
+import { generalTab } from './Search.knobs'
 
 storiesOf('molecules/forms/Search', module).addWithJSX('Default', () => {
-  const placeholder = text('Placeholder', data.placeholder, propsTab)
-  const title = text(
-    'Form Title (visible only for screen readers)',
-    data.title,
-    propsTab
-  )
-  const submitLabel = text(
-    'Submit Button (visible only for screen readers)',
-    data.submitLabel,
-    propsTab
-  )
-  return (
-    <Search placeholder={placeholder} submitLabel={submitLabel} title={title} />
-  )
+  const props = generalTab()
+  return <Search {...props} />
 })
