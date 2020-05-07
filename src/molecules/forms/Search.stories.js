d2235e6b13f210aa059057b364a821295b220450
Eduardo Ludi <eduludi@gmail.com>
2020-05-07 15:40:16 +0200

    Moves Search.knobs.js into SearchStories.js
    (Rollup was complaining for some reason)

--- src/molecules/forms/Search.stories.js
+++ src/molecules/forms/Search.stories.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
 
 import Search from './Search'
+import data from './Search.stories.json'
 
-import { generalTab } from './Search.knobs'
+function getTabData(name, settings = {}) {
+  return {
+    tab: name,
+    ...Search.defaultProps,
+    ...data,
+    ...settings,
+  }
+}
+
+export function generalTab(settings = {}) {
+  const { placeholder, title, submitLabel, tab } = getTabData(
+    'General',
+    settings
+  )
+
+  return {
+    placeholder: text('Placeholder', placeholder, tab),
+    title: text('Form Title (visible only for screen readers)', title, tab),
+    submitLabel: text(
+      'Submit Button (visible only for screen readers)',
+      submitLabel,
+      tab
+    ),
+  }
+}
 
 storiesOf('molecules/forms/Search', module).addWithJSX('Default', () => {
   const props = generalTab()
