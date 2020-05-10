c1785f8d24bf5a4b8fda14dba28a5011534f1813
Eduardo Ludi <eduludi@gmail.com>
2020-05-10 21:41:21 +0200

    Adds ArticleVideo template, its stories and data

---
+++ src/templates/ArticleVideo.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import GridItem from '../atoms/grids/GridItem'
+import GridSeven from '../atoms/grids/GridSeven'
+import ContentBlock from '../molecules/blocks/ContentBlock'
+import HeadingBlock from '../molecules/blocks/HeadingBlock'
+import VideoFigureWithCaption from '../molecules/media/VideoFigureWithCaption'
+import PageContent from '../organisms/content/PageContent'
+import ArticleContentShort from '../organisms/content/ArticleContentShort'
+import BlockFeed from '../organisms/sections/BlockFeed'
+import PageHeaderFeature from '../organisms/sections/PageHeaderFeature'
+import Main from './Main'
+import TemplateWrap from './TemplateWrap'
+
+function ArticleVideo({
+  breadcrumbs,
+  children,
+  caption,
+  category,
+  content,
+  duration,
+  durationLabel,
+  description,
+  date,
+  kicker,
+  segments,
+  segmentsTitle,
+  title,
+  src,
+  ...templateProps
+}) {
+  return (
+    <TemplateWrap {...templateProps}>
+      <Main>
+        <PageHeaderFeature
+          blockType="longform"
+          kicker={kicker}
+          title={title}
+          description={description}
+          date={date}
+          category={category}
+        />
+        <GridSeven
+          as="section"
+          gridWrap="6"
+          noWrapClass
+          shiftSide="left"
+          shiftAt="xxlarge"
+          spacingSize="double"
+          spacingUntil="large"
+          paddingSize="zero"
+          paddingSide="sides"
+        >
+          <GridItem className="c-article" sizeAtL="5" sizeAtXL="4">
+            <VideoFigureWithCaption videoSrc={src} caption={caption} />
+          </GridItem>
+        </GridSeven>
+        <ArticleContentShort
+          breadcrumbs={breadcrumbs}
+          sidebar={
+            <ContentBlock description={`${durationLabel}: ${duration}`} />
+          }
+        >
+          {content || children}
+        </ArticleContentShort>
+        {segments && segments.length > 0 && (
+          <>
+            <HeadingBlock title={segmentsTitle} />
+            <BlockFeed blocks={segments} blocksType="video" grid />
+          </>
+        )}
+      </Main>
+    </TemplateWrap>
+  )
+}
+
+ArticleVideo.propTypes = {
+  breadcrumbs: PageContent.propTypes.breadcrumbs,
+  category: PropTypes.node,
+  children: PropTypes.node,
+  content: PropTypes.node,
+  description: PropTypes.node,
+  duration: PropTypes.string,
+  durationLabel: PropTypes.string,
+  date: PropTypes.node,
+  image: PropTypes.object,
+  kicker: PropTypes.node,
+  segments: PropTypes.array,
+  segmentsTitle: PropTypes.string,
+  title: PropTypes.node,
+  src: PropTypes.string,
+  caption: PropTypes.string,
+}
+ArticleVideo.defaultProps = {
+  breadcrumbs: [],
+  durationLabel: 'Duration',
+  segmentsTitle: 'Program Segments',
+}
+
+export default ArticleVideo
---
+++ src/templates/ArticleVideo.stories.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import {
+  boolean,
+  date as datepicker,
+  object,
+  text,
+} from '@storybook/addon-knobs'
+
+import ArticleVideo from './ArticleVideo'
+
+import Text from '../atoms/texts/Text'
+import { videoFigureWithCaptionTab } from '../molecules/media/VideoFigureWithCaption.stories.js'
+import { Paragraph } from '../helpers/Element'
+
+// Stories and data
+import { globalTab } from './TemplateWrap.stories.js'
+import data from './ArticleVideo.stories.json'
+
+/* Note: This is just a simple demo content. */
+function DemoContent({ title = '', text = '' }) {
+  const paragraphs = text.split('\n')
+  return paragraphs.map((pText, k) => (
+    <Text key={k} hasDropcap={k === 0} spacing>
+      <h2>{title}</h2>
+      <Paragraph>{pText}</Paragraph>
+    </Text>
+  ))
+}
+
+function getTabData(name, settings = {}) {
+  return {
+    tab: name,
+    ...ArticleVideo.defaultProps,
+    ...data,
+    ...settings,
+  }
+}
+
+export function articleHeaderTab(settings = {}) {
+  const { title, description, category, kicker, date, tab } = getTabData(
+    'Header',
+    settings
+  )
+
+  return {
+    title: text('Title ', title, tab),
+    kicker: text('Kicker ', kicker, tab),
+    description: text('Description ', description, tab),
+    date: datepicker('Date ', new Date(date), tab),
+    category: text('Category', category, tab),
+  }
+}
+
+export function videoTab(settings = {}) {
+  const { videoDuration, tab } = getTabData('Video', settings)
+  const { videoSrc, caption } = videoFigureWithCaptionTab(settings)
+
+  return {
+    src: text('Source ', videoSrc, tab),
+    caption: text('Caption', caption, tab),
+    duration: text('Duration', videoDuration, tab),
+  }
+}
+
+export function contentTab(settings = {}) {
+  const { content, tab } = getTabData('Content', settings)
+  return {
+    title: text('Content Title ', content.title, tab),
+    text: text('Content Text', content.text, tab),
+  }
+}
+export function segmentsTab(settings = {}) {
+  const { segmentsTitle, segments, showSegments, tab } = getTabData(
+    'Segments',
+    settings
+  )
+
+  return boolean('Show segments', showSegments, tab)
+    ? {
+        segmentsTitle: text('Title', segmentsTitle, tab),
+        segments: object('Segments', segments, tab),
+      }
+    : null
+}
+
+export function articleTabs(settings = {}) {
+  return {
+    ...articleHeaderTab(settings),
+    ...videoTab(settings),
+    ...segmentsTab(settings),
+    content: contentTab(settings),
+    ...globalTab(settings),
+  }
+}
+
+storiesOf('templates/ArticleVideo', module)
+  .addWithJSX('Default', () => {
+    const { content, ...props } = articleTabs()
+
+    return (
+      <ArticleVideo {...props}>
+        <DemoContent {...content} />
+      </ArticleVideo>
+    )
+  })
+  .addWithJSX('With segments', () => {
+    const { content, ...props } = articleTabs({ showSegments: true })
+
+    return (
+      <ArticleVideo {...props}>
+        <DemoContent {...content} />
+      </ArticleVideo>
+    )
+  })
