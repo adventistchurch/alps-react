9c33b6f67016d05d919180c6443b2828d206ec8a
Eduardo Ludi <eduludi@gmail.com>
2020-06-17 15:44:58 +0200

    Makes icons and logos lazy loaded

--- src/atoms/images/logos/index.js
+++ src/atoms/images/logos/index.js
@@ -1,7 +1,14 @@
-import Gain from './Gain'
-import SDA from './SDA'
-import SDAArabic from './SDAArabic'
-import SDAWithIcon from './SDAWithIcon'
+// import Gain from './Gain'
+// import SDA from './SDA'
+// import SDAArabic from './SDAArabic'
+// import SDAWithIcon from './SDAWithIcon'
+
+import { lazy } from 'react'
+
+const Gain = lazy(() => import('./Gain'))
+const SDA = lazy(() => import('./SDA'))
+const SDAArabic = lazy(() => import('./SDAArabic'))
+const SDAWithIcon = lazy(() => import('./SDAWithIcon'))
 
 const logos = {
   gain: Gain,
