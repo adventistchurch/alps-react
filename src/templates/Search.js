68bdf41e494f9cbf7bf8016d9fbaa1e921e311a6
Eduardo Ludi <eduludi@gmail.com>
2020-06-24 15:03:18 +0200

    Improves Search template

--- src/templates/Search.js
+++ src/templates/Search.js
@@ -1,19 +1,29 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 import SearchHeader from '../molecules/forms/SearchHeader'
 import SearchResults from '../organisms/content/SearchResults'
 import BlankTemplate from './BlankTemplate'
+import ArticleContent from '../organisms/content/ArticleContent'
 
-function Search() {
+function Search({ results, searchHeader, ...props }) {
   return (
-    <BlankTemplate>
-      <SearchHeader />
-      <SearchResults />
+    <BlankTemplate {...props}>
+      <SearchHeader {...searchHeader} />
+      <ArticleContent>
+        <SearchResults results={results} />
+      </ArticleContent>
     </BlankTemplate>
   )
 }
 
-Search.propTypes = {}
+Search.propTypes = {
+  results: PropTypes.array,
+  searchHeader: PropTypes.object,
+}
+Search.defaultProps = {
+  results: [],
+  searchHeader: {},
+}
 
 export default Search
--- src/templates/Search.stories.js
+++ src/templates/Search.stories.js
@@ -1,8 +1,53 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { object } from '@storybook/addon-knobs'
+
+import { useGlobalTab } from './TemplateWrap.stories.js'
+import { pageHeaderTab } from '../organisms/sections/PageHeader.stories.js'
+import { useSearchHeaderTab } from '../molecules/forms/SearchHeader.stories.js'
+import data from './Search.stories.json'
 
 import Search from './Search'
 
-storiesOf('templates/Search', module).addWithJSX('Default', () => {
-  return <Search />
-})
+function getTabData(name, settings = {}) {
+  return {
+    tab: name,
+    ...Search.defaultProps,
+    ...data,
+    ...settings,
+  }
+}
+
+export { pageHeaderTab }
+
+export function resultsTab(settings = {}) {
+  const { results, tab } = getTabData('Results', settings)
+
+  return {
+    results: object('Items', results, tab),
+  }
+}
+
+export function useSearchTabs(settings = {}) {
+  const { pageHeader } = getTabData(null, settings)
+  const searchHeader = useSearchHeaderTab(settings)
+
+  return {
+    pageHeader: pageHeaderTab(pageHeader),
+    searchHeader: searchHeader,
+    ...resultsTab(settings),
+    ...useGlobalTab(settings),
+  }
+}
+
+storiesOf('templates/Search', module)
+  .addWithJSX('Default', () => {
+    const props = useSearchTabs()
+
+    return <Search {...props} />
+  })
+  .addWithJSX('with suggestions', () => {
+    const props = useSearchTabs({ withSuggestions: true, defaultTerm: 'Hi' })
+
+    return <Search {...props} />
+  })
