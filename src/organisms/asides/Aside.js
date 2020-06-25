149ba43124ae60db4355d5a5555bbc4828f0001f
Eduardo Ludi <eduludi@gmail.com>
2020-06-25 16:03:22 +0200

    Refactors Aside to only accept children
    - User can provide everything now, in the order they want

--- src/organisms/asides/Aside.js
+++ src/organisms/asides/Aside.js
@@ -2,38 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { Div } from '../../helpers/Element'
-import SearchFilter from '../../molecules/forms/SearchFilter'
-import BlockWrap from './BlockWrap'
 
-function Aside({ content, contentPosition, primary, secondary, search }) {
+function Aside({ children }) {
   return (
     <Div spacingSize="double" paddingSide="right">
-      {contentPosition === 'top' && content}
-      {search && <SearchFilter {...search} />}
-      {contentPosition === 'afterSearch' && content}
-      {primary && <BlockWrap type="mediaBlock" {...primary} />}
-      {contentPosition === 'afterPrimary' && content}
-      {secondary && <BlockWrap type="contentBlock" {...secondary} />}
-      {contentPosition === 'bottom' && content}
+      {children}
     </Div>
   )
 }
 
 Aside.propTypes = {
-  primary: PropTypes.shape(BlockWrap.propTypes),
-  secondary: PropTypes.shape(BlockWrap.propTypes),
-  search: PropTypes.shape(SearchFilter.propTypes),
-  content: PropTypes.node,
-  contentPosition: PropTypes.oneOf([
-    'top',
-    'afterSearch',
-    'afterPrimary',
-    'bottom',
-  ]),
-}
-
-Aside.defaultProps = {
-  contentPosition: 'top',
+  children: PropTypes.node,
 }
 
 export default Aside
--- src/organisms/asides/Aside.stories.js
+++ src/organisms/asides/Aside.stories.js
@@ -1,36 +1,62 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { object } from '@storybook/addon-knobs'
+import { object, text } from '@storybook/addon-knobs'
 
 import Aside from './Aside'
+import BlockWrap from './BlockWrap'
+
 import data from './Aside.stories.json'
 
 function getTabData(name, settings = {}) {
+  const { parentTab, ...others } = settings
+
   return {
-    tab: name,
+    tab: `${parentTab ? `${parentTab}: ` : ''}${name}`,
     ...Aside.defaultProps,
     ...data,
-    ...settings,
+    ...others,
   }
 }
 
-// TODO: Add search and content knobs
-export function asideTab(settings = {}) {
-  const { primary, secondary, hidePrimary, hideSecondary, tab } = getTabData(
-    'Aside',
-    settings
-  )
+function mediaBlocksTab(settings = {}) {
+  const { media, tab } = getTabData('Media Blocks', settings)
 
   return {
-    primary: hidePrimary ? null : object('Primary Blocks *', primary, tab),
-    secondary: hideSecondary
-      ? null
-      : object('Secondary Blocks', secondary, tab),
+    title: text('Title', media.title, tab),
+    linkLabel: text('Link label', media.linkLabel, tab),
+    linkUrl: text('Link URL', media.linkUrl, tab),
+    items: object('Items', media.items, tab),
+  }
+}
+
+function commentsBlocksTab(settings = {}) {
+  const { comments, tab } = getTabData('Comment Blocks', settings)
+
+  return {
+    title: text('Title', comments.title, tab),
+    linkLabel: text('Link label', comments.linkLabel, tab),
+    linkUrl: text('Link URL', comments.linkUrl, tab),
+    items: object('Items', comments.items, tab),
+  }
+}
+
+export function asideTab(settings = {}) {
+  const defaultSettings = { parentTab: 'Aside' }
+  const mediaProps = mediaBlocksTab({ ...defaultSettings, ...settings })
+  const commentsProps = commentsBlocksTab({ ...defaultSettings, ...settings })
+
+  return {
+    children: (
+      <>
+        <BlockWrap type="mediaBlock" {...mediaProps} />
+        <BlockWrap type="contentBlock" {...commentsProps} />
+      </>
+    ),
   }
 }
 
 storiesOf('organisms/asides/Aside', module).addWithJSX('Default', () => {
-  const props = asideTab()
+  const props = asideTab({ parentTab: '' })
 
   return <Aside {...props} />
 })
