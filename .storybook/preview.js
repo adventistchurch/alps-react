ed5902408860c4d9f4892b76881a86b50e561a3e
Eduardo Ludi <eduludi@gmail.com>
2020-07-14 11:11:12 +0200

    Fixes bug with decorators on .storybook/preview.js
    - Fixes two decorators being added at once
    - Imports extracted withAlpsWrapper()
    - Moves withHTML before withAlpsWrapper

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,33 +1,18 @@
-import React from 'react'
-
 import {
   configure,
   addDecorator,
   addParameters,
   setAddon,
 } from '@storybook/react'
-import { boolean, select, withKnobs } from '@storybook/addon-knobs'
+import { withKnobs } from '@storybook/addon-knobs'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { withHTML } from '@whitespace/storybook-addon-html/react'
 import JSXAddon from 'storybook-addon-jsx'
 
-import Wrapper from '../src/Wrapper'
-import { primaryColors, secondaryColors } from '../src/atoms/global/colors'
-
 import theme from './theme'
+import withAlpsWrapper from './withAlpsWrapper'
 
-addDecorator(story => {
-  const content = story()
-  const tab = 'Theme' // Tab Name
-  const wrapperProps = {
-    hasGrid: boolean('Has Grid', true, tab),
-    primaryColor: select('Primary color', primaryColors, 'ming', tab),
-    secondaryColor: select('Secondary color', secondaryColors, null, tab),
-  }
-
-  return <Wrapper {...wrapperProps}>{content}</Wrapper>
-})
-
+// IMPORTANT: this must be added BEFORE alps wrapper decorator
 addDecorator(
   withHTML({
     prettier: {
@@ -36,11 +21,18 @@ addDecorator(
       jsxBracketSameLine: true,
       htmlWhitespaceSensitivity: 'ignore',
     },
-  }),
+  })
+)
+
+// Decorates story with current theme and grid wrapper
+addDecorator(withAlpsWrapper())
+
+// IMPORTANT: this must be added AFTER alps wrapper decorator
+addDecorator(
   withKnobs({
     escapeHTML: false,
   })
-) // this must be added after the story decorator
+)
 
 setAddon(JSXAddon)
 
