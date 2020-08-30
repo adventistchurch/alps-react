69b584eae66eb8bce1cb40f56cd1a1a0548ae288
Eduardo Ludi <eduludi@gmail.com>
2020-08-30 18:11:57 +0200

    Converts atoms/icons/Icon stories to CSF

--- src/atoms/icons/Icon.stories.js
+++ src/atoms/icons/Icon.stories.js
@@ -1,35 +1,76 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { select } from '@storybook/addon-knobs'
 import Icon, { iconNames } from './Icon'
 
-const propsTab = 'Props'
+export default {
+  title: 'Atoms/Icons/Icon',
+  component: Icon,
+  args: {
+    name: '',
+  },
+}
 
-storiesOf('atoms/icons/Icon', module)
-  .addWithJSX('Default', () => {
-    const name = select('Name *', iconNames, 'logo', propsTab)
-    return <Icon name={name} />
-  })
+export const Default = Icon.bind({})
+Default.args = { name: 'logo' }
+Default.argTypes = { name: { control: { type: 'select', options: iconNames } } }
 
-  .addWithJSX('arrow-bracket-left', () => <Icon name="arrow-bracket-left" />)
-  .addWithJSX('arrow-bracket-right', () => <Icon name="arrow-bracket-right" />)
-  .addWithJSX('arrow-long-left', () => <Icon name="arrow-long-left" />)
-  .addWithJSX('arrow-long-right', () => <Icon name="arrow-long-right" />)
-  .addWithJSX('arrow-short-left', () => <Icon name="arrow-short-left" />)
-  .addWithJSX('arrow-short-right', () => <Icon name="arrow-short-right" />)
-  .addWithJSX('close', () => <Icon name="close" />)
-  .addWithJSX('contact', () => <Icon name="contact" />)
-  .addWithJSX('dots', () => <Icon name="dots" />)
-  .addWithJSX('find', () => <Icon name="find" />)
-  .addWithJSX('heart', () => <Icon name="heart" />)
-  .addWithJSX('language', () => <Icon name="language" />)
-  .addWithJSX('legal', () => <Icon name="legal" />)
-  .addWithJSX('logo', () => <Icon name="logo" />)
-  .addWithJSX('logo-dark', () => <Icon name="logo-dark" />)
-  .addWithJSX('logo-round', () => <Icon name="logo-round" />)
-  .addWithJSX('menu', () => <Icon name="menu" />)
-  .addWithJSX('plus', () => <Icon name="plus" />)
-  .addWithJSX('search', () => <Icon name="search" />)
-  .addWithJSX('settings', () => <Icon name="settings" />)
-  .addWithJSX('share', () => <Icon name="share" />)
-  .addWithJSX('subscribe', () => <Icon name="subscribe" />)
+export const ArrowBracketLeft = Icon.bind({})
+ArrowBracketLeft.args = { name: 'arrow-bracket-left' }
+
+export const ArrowBracketRight = Icon.bind({})
+ArrowBracketRight.args = { name: 'arrow-bracket-right' }
+
+export const ArrowLongLeft = Icon.bind({})
+ArrowLongLeft.args = { name: 'arrow-long-left' }
+
+export const ArrowLongRight = Icon.bind({})
+ArrowLongRight.args = { name: 'arrow-short-right' }
+
+export const ArrowShortLeft = Icon.bind({})
+ArrowShortLeft.args = { name: 'arrow-short-left' }
+
+export const ArrowShortRight = Icon.bind({})
+ArrowShortRight.args = { name: 'arrow-short-right' }
+
+export const Close = Icon.bind({})
+Close.args = { name: 'close' }
+
+export const Contact = Icon.bind({})
+Contact.args = { name: 'contact' }
+
+export const Dots = Icon.bind({})
+Dots.args = { name: 'dots' }
+
+export const Find = Icon.bind({})
+Find.args = { name: 'find' }
+
+export const Heart = Icon.bind({})
+Heart.args = { name: 'heart' }
+
+export const Language = Icon.bind({})
+Language.args = { name: 'language' }
+
+export const Logo = Icon.bind({})
+Logo.args = { name: 'logo' }
+
+export const LogoDark = Icon.bind({})
+LogoDark.args = { name: 'logo-dark' }
+
+export const LogoRound = Icon.bind({})
+LogoRound.args = { name: 'logo-round' }
+
+export const Menu = Icon.bind({})
+Menu.args = { name: 'menu' }
+
+export const Plus = Icon.bind({})
+Plus.args = { name: 'plus' }
+
+export const Search = Icon.bind({})
+Search.args = { name: 'search' }
+
+export const Settings = Icon.bind({})
+Settings.args = { name: 'settings' }
+
+export const Share = Icon.bind({})
+Share.args = { name: 'share' }
+
+export const Subscribe = Icon.bind({})
+Subscribe.args = { name: 'subscribe' }
