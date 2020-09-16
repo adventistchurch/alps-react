371b7fc7e25da7b1d79997ef8cfc08023167cef6
Eduardo Ludi <eduludi@gmail.com>
2020-09-16 09:52:46 +0200

    Adds intersection-observer polyfill to AlpsContextProvider

--- src/AlpsContextProvider.js
+++ src/AlpsContextProvider.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import 'intersection-observer' // Polyfill required for react-lazy-images
+
 import { DrawerContextProvider } from './helpers/useDrawerContext'
 import { LinkContextProvider } from './helpers/useLinkContext'
 
