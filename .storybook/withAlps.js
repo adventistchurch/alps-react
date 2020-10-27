796ae259c7a1b4a8b1636f564f535ddac11ee150
Eduardo Ludi <eduludi@gmail.com>
2020-10-27 10:42:51 +0100

    Adds missing body class in withAlpsTheme
    - This fixes an issue with body class not being present in storybook preview

--- .storybook/withAlps.js
+++ .storybook/withAlps.js
@@ -24,7 +24,7 @@ export function withAlpsTheme({
       components: [ThemeWrap],
       params: primaryColors.map(color => ({
         name: color,
-        props: { color },
+        props: { color, className: 'body' },
         default: color === primaryColor,
       })),
       options: {
