897400680ffa5dda09ceb777970ec3858b26c72c
Eduardo Ludi <eduludi@gmail.com>
2021-04-12 13:42:51 +0200

    Refactors alps url in getExternalAssets()

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ export function getExternalAssets({
   version = '3.7.9',
   theme = 'bluejay',
 } = {}) {
+  // Sets suffix for ALPS CSS theme (if `theme` provided)
+  const themeSuffix = theme ? `-${theme}` : ''
+
   return {
     css: [
       '//fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i|Noto+Serif:400,400i,700,700i&display=swap',
-      `//cdn.adventist.org/alps/3/${version}/css/main${
-        theme ? `-${theme}` : ''
-      }.css`,
+      `//cdn.adventist.org/alps/3/${version}/css/main${themeSuffix}.css`,
     ],
   }
 }
